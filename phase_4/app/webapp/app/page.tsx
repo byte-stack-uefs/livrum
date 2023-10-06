4ee56602088caeeb6e0ab61427ccf26eb8455cb6
Almir Neto <almirneto338@gmail.com>
2023-10-06 00:21:22 -0300

    Utilizando variável para exebução do nome do sistema

--- phase_4/app/webapp/app/page.tsx
+++ phase_4/app/webapp/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <p className="text-emerald-500">Welcome to livrum.</p>
-            <p className="bg-red-400 p-2 rounded-lg">Livrum PBL</p>
+            <p className="text-emerald-500">Welcome to {process.env.APP_NAME}.</p>
+            <p className="bg-red-400 p-2 rounded-lg">{process.env.APP_NAME} PBL</p>
         </main>
     );
 }
