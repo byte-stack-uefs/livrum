0fce3eda65329db992760aee062262d96c832133
Almir Neto <almirneto338@gmail.com>
2023-10-06 15:49:39 -0300

    Movendo código para componentes

--- phase_4/app/webapp/app/components/layouts/PublicLayout.tsx
+++ phase_4/app/webapp/app/components/layouts/PublicLayout.tsx
@@ -6,31 +6,15 @@
  * Além disso, ele será utilizado nas páginas do cliente (é o mesmo layout com um menu lateral)
  */
 
-import Image from "next/image";
+import TopBar from "../TopBar";
+import PublicFooter from "../Footer";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
-            <section className="bg-white">
-                <div className="flex justify-between">
-                    <div>
-                        <Image alt="Livrum TopBar Logo" src="/livrum-lateral.png" width={300} height={120} />
-                    </div>
-                    <div className="bg-gray-300">Campo de busca</div>
-                    <div className="flex justify-evenly">
-                        <div className="text-livrum-dark bold">Ícone Carrinho</div>
-                        <div>Ícone usuário</div>
-                    </div>
-                </div>
-            </section>
+            <TopBar />
             <section>{children}</section>
-            <footer className="bg-livrum-primary text-center text-white">
-                <p className="p-2">Fale Conosco</p>
-                <p className="p-1">{process.env.APP_EMAIL}</p>
-                <p className="p-1">
-                    By {process.env.COMPANY} &copy; - {new Date().getFullYear()}{" "}
-                </p>
-            </footer>
+            <PublicFooter />
         </div>
     );
 }
