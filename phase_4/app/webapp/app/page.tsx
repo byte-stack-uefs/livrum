9fe40f1a95ee5456adf049e8e552dc2891842d9d
Almir Neto <almirneto338@gmail.com>
2023-10-06 00:11:44 -0300

    Testando algumas classes do tailwind na página inicial

--- phase_4/app/webapp/app/page.tsx
+++ phase_4/app/webapp/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <p className="text-emerald-500">Welcome to livrum.</p>
-            <p className="bg-red-400">Livrum PBL</p>
+            <p className="bg-red-400 p-2 rounded-lg">Livrum PBL</p>
         </main>
     );
 }
