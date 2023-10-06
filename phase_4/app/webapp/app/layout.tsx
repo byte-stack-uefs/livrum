e41a2de52ca4c767bebd90c74657145ce70f4859
Almir Neto <almirneto338@gmail.com>
2023-10-06 14:27:18 -0300

    Utilizando o layout públicas nas páginas públicas

--- phase_4/app/webapp/app/layout.tsx
+++ phase_4/app/webapp/app/layout.tsx
@@ -2,12 +2,14 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Source_Sans_3 } from "next/font/google";
 
+import PublicLayout from "./components/layouts/PublicLayout";
+
 const font = Source_Sans_3({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
     title: {
         template: "%s | " + process.env.APP_NAME,
-        default: process.env.APP_NAME + '',
+        default: process.env.APP_NAME + "",
     },
     description: "Livrum website",
 };
@@ -15,7 +17,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={font.className + " min-h-screen"}>{children}</body>
+            <body className={font.className + " min-h-screen"}>
+                <PublicLayout>{children}</PublicLayout>
+            </body>
         </html>
     );
 }
