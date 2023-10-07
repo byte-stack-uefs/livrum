e6f5882a7c6b4a236cb117b1a3b5e1537a00f546
Almir Neto <almirneto338@gmail.com>
2023-10-07 17:55:58 -0300

    - Adicionando divisão entre elementos da barra de descrição - Alinhando "ícones"

--- phase_4/app/webapp/app/components/TopBar.tsx
+++ phase_4/app/webapp/app/components/TopBar.tsx
@@ -15,17 +15,17 @@ export default function TopBar() {
                         </p>
                     </div>
                     <div className="bg-gray-300 text-center self-center w-1/3">Campo de busca</div>
-                    <div className="flex justify-evenly text-livrum-dark font-bold w-1/3">
+                    <div className="flex justify-evenly text-livrum-dark font-bold w-1/3 items-center">
                         <div>Ícone Carrinho</div>
                         <div>Ícone usuário</div>
                     </div>
                 </div>
             </section>
             <section className="bg-livrum-primary text-white h-12">
-                <div className="flex justify-around h-full items-center">
-                    <div>Mais Econômico</div>
-                    <div>Super Rápido</div>
-                    <div>Sustentável</div>
+                <div className="flex justify-around h-full items-center divide-x">
+                    <div className="w-1/3 text-center">Mais Econômico</div>
+                    <div className="w-1/3 text-center">Super Rápido</div>
+                    <div className="w-1/3 text-center">Sustentável</div>
                 </div>
             </section>
         </div>
