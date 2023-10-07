6d4d2fe12b6dbd51f84d0500bf25751626168087
Almir Neto <almirneto338@gmail.com>
2023-10-07 17:59:05 -0300

    Removendo código teste da página principal

--- phase_4/app/webapp/app/page.tsx
+++ phase_4/app/webapp/app/page.tsx
@@ -6,9 +6,10 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <p className="text-emerald-500">Welcome to {process.env.APP_NAME}.</p>
-            <p className="bg-red-400 p-2 rounded-lg">{process.env.APP_NAME} PBL</p>
+        <main className="flex min-h-screen flex-col items-center justify-center">
+            <p style={{ fontSize: 100, fontWeight: 'bold', textTransform: 'uppercase' }}>
+                {process.env.APP_NAME}
+            </p>
         </main>
     );
 }
