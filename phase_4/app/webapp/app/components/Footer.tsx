0a77590f519edeac553c855c076dadc6502c5c39
Almir Neto <almirneto338@gmail.com>
2023-10-12 00:07:58 -0300

    Documentando componente do rodapé público do sistema

--- phase_4/app/webapp/app/components/Footer.tsx
+++ phase_4/app/webapp/app/components/Footer.tsx
@@ -1,11 +1,14 @@
-
-
+/**
+ * Esse componente é responsável por renderizar o Rodapé da área pública do sistema
+ */
 export default function PublicFooter() {
-    return (<footer className="bg-livrum-primary text-center text-white">
-        <p className="p-2">Fale Conosco</p>
-        <p className="p-1">{process.env.APP_EMAIL}</p>
-        <p className="p-1">
-            By {process.env.COMPANY} &copy; - {new Date().getFullYear()}{" "}
-        </p>
-    </footer>)
+    return (
+        <footer className="bg-livrum-primary text-center text-white">
+            <p className="p-2">Fale Conosco</p>
+            <p className="p-1">{process.env.APP_EMAIL}</p>
+            <p className="p-1">
+                By {process.env.COMPANY} &copy; - {new Date().getFullYear()}{" "}
+            </p>
+        </footer>
+    );
 }
