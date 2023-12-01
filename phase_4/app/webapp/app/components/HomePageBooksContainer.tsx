9a9a050c5e10c9219499bbf86a708827cc15e5ac
Almir Neto <almirneto338@gmail.com>
2023-11-30 21:54:16 -0300

    Adaptando container de ebooks da página inicial para utilizar um slider

--- phase_4/app/webapp/app/components/HomePageBooksContainer.tsx
+++ phase_4/app/webapp/app/components/HomePageBooksContainer.tsx
@@ -1,11 +1,14 @@
 "use client";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import { Box, Button, Grid, Typography } from "@mui/material";
 
+import "swiper/css";
 import Image from "next/image";
+import "swiper/css/navigation";
 import Divider from "./Divider";
-
 import { theme } from "@/app/theme";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 function ContainerBookCard({ book }: { book: any }) {
     return (
@@ -34,7 +37,7 @@ function ContainerBookCard({ book }: { book: any }) {
 
 export default function HomePageBooksContainer({ title = "", books = [] }: { title: string; books: Array<any> }) {
     return (
-        <Box sx={{ textAlign: "center" }}>
+        <Box sx={{ textAlign: "center", marginBottom: 5 }}>
             <Typography variant="h4" sx={{ fontWeight: "bold", color: theme.palette.darker.main }}>
                 {title}
             </Typography>
@@ -42,13 +45,15 @@ export default function HomePageBooksContainer({ title = "", books = [] }: { tit
             <Divider theme={theme} width="5%" />
 
             <Grid container sx={{ marginTop: 4 }}>
-                {books.map((e) => {
-                    return (
-                        <Grid key={e} item xs={12} md={4}>
-                            <ContainerBookCard book={e} />
-                        </Grid>
-                    );
-                })}
+                <Swiper loop={true} slidesPerView={4} spaceBetween={20} navigation={true} modules={[Navigation]}>
+                    {books.map((e) => {
+                        return (
+                            <SwiperSlide key={e}>
+                                <ContainerBookCard book={e} />
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
             </Grid>
         </Box>
     );
