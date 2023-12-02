fd80a2a8196a7fd02f7da1ca78fd44f80238b7ba
Almir Neto <almirneto338@gmail.com>
2023-12-02 14:07:35 -0300

    Adicionando layout base das páginas internas (admin e autor)

---
+++ phase_4/app/webapp/app/components/layouts/InternalLayout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import { Container, Paper } from "@mui/material";
+
+import { theme } from '@/app/theme'
+
+export default function InternalLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <Container maxWidth={false} disableGutters={true} sx={{
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: theme.palette.dark.main,
+        }}>
+            <Paper elevation={0} sx={{
+                width: '90%',
+                height: '85%',
+                borderRadius: 6,
+                backgroundColor: '#F1F1F1'
+            }}>
+                {children}
+            </Paper>
+        </Container>
+    );
+}
