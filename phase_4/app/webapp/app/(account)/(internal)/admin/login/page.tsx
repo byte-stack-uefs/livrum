76117f1f69b06a52169cc2800f3cc5e90c29018b
Almir Neto <almirneto338@gmail.com>
2023-12-05 14:24:49 -0300

    Ajustando a página de login de administrador

--- phase_4/app/webapp/app/(account)/(internal)/admin/login/page.tsx
+++ phase_4/app/webapp/app/(account)/(internal)/admin/login/page.tsx
@@ -1,3 +1,93 @@
-export default function Page() {
-    return <p>Lindo</p>;
-}
+"use client";
+
+import React, { useState } from "react";
+import { TextField, Button, Paper, Typography, Link, FormControl } from "@mui/material";
+
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { theme } from "@/app/theme";
+
+const AdminLogin = () => {
+    const [value, setValue] = useState(0);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleSubmitClient = (event) => {
+        event.preventDefault();
+        console.log("Email:", email);
+        console.log("Senha:", password);
+    };
+
+    return (
+        <Grid container>
+            <Grid xs={11} md={4} margin="auto">
+                <Paper>
+                    <FormControl>
+                        <Grid container padding={3} spacing={2} justifyContent="center" alignItems="center" style={{ textAlign: "center" }}>
+                            <Grid xs={12}>
+                                <Typography variant="h5" fontWeight="bold">
+                                    Acesso administrador
+                                </Typography>
+                                <Typography variant="caption" color="GrayText">
+                                    Seja bem vindo de volta!
+                                </Typography>
+                            </Grid>
+                            <Grid container spacing={2} xs={12} py={3}>
+                                <Grid xs={12}>
+                                    <TextField
+                                        label="E-mail"
+                                        variant="outlined"
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        fullWidth
+                                        size="small"
+                                    />
+                                </Grid>
+                                <Grid xs={12}>
+                                    <TextField
+                                        label="Senha"
+                                        variant="outlined"
+                                        value={password}
+                                        type="password"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        fullWidth
+                                        size="small"
+                                    />
+                                </Grid>
+                            </Grid>
+
+                            <Grid xs={12} style={{ textAlign: "right", alignSelf: "flex-start" }}>
+                                <Link
+                                    component="button"
+                                    variant="body2"
+                                    underline="none"
+                                    onClick={() => {
+                                        /*
+                                    Direcionar para a página de recuperação!!!!
+                                */
+                                    }}
+                                    style={{
+                                        color: theme.palette.primary.main,
+                                    }}
+                                >
+                                    Esqueceu a senha?
+                                </Link>
+                            </Grid>
+                            <Grid xs={12} md={6}>
+                                <Button fullWidth type="submit" variant="contained" color="darker">
+                                    Login
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </FormControl>
+                </Paper>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default AdminLogin;
