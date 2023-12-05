1a3b66621a9e2547b3b815e77b9879abc0491c70
Almir Neto <almirneto338@gmail.com>
2023-12-05 13:32:51 -0300

    Melhorando o layout de 'conta'

--- phase_4/app/webapp/app/components/layouts/AccountLayout.tsx
+++ phase_4/app/webapp/app/components/layouts/AccountLayout.tsx
@@ -1,17 +1,18 @@
 import Footer from "../Footer";
+import { Stack } from "@mui/material";
 import AccountHeader from "../AccountHeader";
-import { Box, Container, Grid } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
-            <Container maxWidth="md" sx={{ marginTop: 8 }}>
-                <Grid sx={{ display: "flex", justifyContent: "center" }}>
-                    <AccountHeader />
-                </Grid>
-            </Container>
-            <Box marginY={4}>{children}</Box>
-            <Footer />
-        </div>
+        <Stack direction="column" justifyContent="space-between" minHeight="100vh">
+            <Grid xs={12} sx={{ display: "flex", justifyContent: "center", paddingTop: 8 }}>
+                <AccountHeader />
+            </Grid>
+            <Grid xs={12} py={4}>{children}</Grid>
+            <Grid xs={12}>
+                <Footer />
+            </Grid>
+        </Stack>
     );
 }
