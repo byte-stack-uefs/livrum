e0fc2df80db8ac9a66ef89261aea868c4104b5e4
Almir Neto <almirneto338@gmail.com>
2023-12-05 14:50:50 -0300

    Corrigir layout interno

--- phase_4/app/webapp/app/components/layouts/InternalLayout.tsx
+++ phase_4/app/webapp/app/components/layouts/InternalLayout.tsx
@@ -10,103 +10,129 @@ import { Avatar, Button, Container, Grid, Paper, Typography } from "@mui/materia
 import { Discount, Groups, Home, LibraryBooks, Logout, Person, SubdirectoryArrowRight } from "@mui/icons-material";
 
 export default function InternalLayout({ children }: { children: React.ReactNode }) {
-
     const name = "Username";
     const email = "email@gmail.com";
-    const avatarSrc = 'https://st2.depositphotos.com/1104517/11967/v/950/depositphotos_119675554-stock-illustration-male-avatar-profile-picture-vector.jpg';
+    const avatarSrc =
+        "https://st2.depositphotos.com/1104517/11967/v/950/depositphotos_119675554-stock-illustration-male-avatar-profile-picture-vector.jpg";
     const [userLevel, setUserLevel] = useState(UserLevel.ADMIN);
 
     const buttons: LivrumButtonMenuItems[] = [
         {
-            label: 'Minha Conta',
+            label: "Minha Conta",
             icon: <Person />,
-            visible: true
+            visible: true,
         },
         {
             label: "Painel",
             icon: <Home />,
-            visible: true
+            visible: true,
         },
         {
             label: "Obras",
             icon: <LibraryBooks />,
-            visible: userLevel == UserLevel.ADMIN
+            visible: userLevel == UserLevel.ADMIN,
         },
         {
             label: "Minhas Obras",
             icon: <LibraryBooks />,
-            visible: userLevel == UserLevel.AUTHOR
+            visible: userLevel == UserLevel.AUTHOR,
         },
         {
             label: "Usuários",
             icon: <Groups />,
-            visible: userLevel == UserLevel.ADMIN
+            visible: userLevel == UserLevel.ADMIN,
         },
         {
             label: "Cupons",
             icon: <Discount />,
-            visible: true
+            visible: true,
         },
         {
             label: "Submeter",
             icon: <SubdirectoryArrowRight />,
-            visible: userLevel == UserLevel.AUTHOR
-        }
+            visible: userLevel == UserLevel.AUTHOR,
+        },
     ];
 
     function handleLogout() {
-        window.location.replace('/login')
+        window.location.replace("/login");
     }
 
     return (
-        <Container maxWidth={false} disableGutters={true} sx={{
-            height: '100vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#1E3345'
-        }}>
-            <Paper elevation={0} sx={{
-                width: '90%',
-                height: '85%',
-                borderRadius: 6,
-                overflow: 'hidden',
-                backgroundColor: '#F1F1F1',
-            }}>
-
-                <Grid container sx={{
-                    height: '100%'
-                }}>
-                    <Grid sx={{
-                        height: '100%',
-                        backgroundColor: '#fff',
-                        paddingTop: 2
-                    }} sm={3} item>
-
-                        <Grid container sx={{ height: '100%' }}>
-                            <Grid item xs={12} >
-                                <Link href={userLevel == UserLevel.ADMIN ? '/admin' : '/autor'} style={{ display: 'flex', justifyContent: 'space-evenly', textDecoration: 'none' }}>
+        <Container
+            maxWidth={false}
+            disableGutters={true}
+            sx={{
+                height: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#1E3345",
+            }}
+        >
+            <Paper
+                elevation={0}
+                sx={{
+                    width: "90%",
+                    height: "85%",
+                    borderRadius: 6,
+                    overflow: "hidden",
+                    backgroundColor: "#F1F1F1",
+                }}
+            >
+                <Grid
+                    container
+                    sx={{
+                        height: "100%",
+                    }}
+                >
+                    <Grid
+                        sx={{
+                            height: "100%",
+                            backgroundColor: "#fff",
+                            paddingTop: 2,
+                        }}
+                        sm={3}
+                        item
+                    >
+                        <Grid container sx={{ height: "100%" }}>
+                            <Grid item xs={12}>
+                                <Link
+                                    href={userLevel == UserLevel.ADMIN ? "/admin" : "/autor"}
+                                    style={{ display: "flex", justifyContent: "space-evenly", textDecoration: "none" }}
+                                >
                                     <AccountHeader logoScale={0.15} fontSize={32} />
                                 </Link>
                             </Grid>
-                            <Grid item xs={12} sx={{ textAlign: 'center', paddingY: 1.5 }}>
-                                <Avatar sx={{ margin: 'auto', width: 56, height: 56 }} src={avatarSrc} />
-                                <Typography variant="h5" color="darker">{name}</Typography>
-                                <Typography variant="body2" color="darker">{email}</Typography>
+                            <Grid item xs={12} sx={{ textAlign: "center", paddingY: 1.5 }}>
+                                <Avatar sx={{ margin: "auto", width: 56, height: 56 }} src={avatarSrc} />
+                                <Typography variant="h5" color="darker">
+                                    {name}
+                                </Typography>
+                                <Typography variant="body2" color="darker">
+                                    {email}
+                                </Typography>
                             </Grid>
                             <Grid item xs={12}>
                                 <LivrumButtonMenu buttons={buttons} />
                             </Grid>
-                            <Grid item xs={12} sx={{ alignSelf: 'flex-end' }}>
-                                <Button size="large" startIcon={<Logout />} fullWidth color="dark" onClick={(e) => { handleLogout() }}>
+                            <Grid item xs={12} sx={{ alignSelf: "flex-end" }}>
+                                <Button
+                                    size="large"
+                                    startIcon={<Logout />}
+                                    fullWidth
+                                    color="dark"
+                                    onClick={(e) => {
+                                        handleLogout();
+                                    }}
+                                >
                                     Sair
                                 </Button>
                             </Grid>
                         </Grid>
-
                     </Grid>
                     <Grid item sm={9}>
-                        {children}
+                        <Container maxWidth={false}>{children}</Container>
                     </Grid>
                 </Grid>
             </Paper>
