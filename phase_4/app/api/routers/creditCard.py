107892cd1a09369c8d4497f380e214e8a22d62f4
Almir Neto <almirneto338@gmail.com>
2023-12-06 03:05:02 -0300

    Permitindo que somente clientes possam acessar os recursos de cartão de crédito

--- phase_4/app/api/routers/creditCard.py
+++ phase_4/app/api/routers/creditCard.py
@@ -1,21 +1,23 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
-from models.user import User
 from dependencies import security
+from models.user import User, UserType
+from fastapi import APIRouter, Depends
 
 router = APIRouter(prefix="/credit-card", tags=["Credit Card"])
 
+access = security.UserHasAccess([UserType.CUSTOMER])
+
 
 @router.post("/", description="Create a Credit Card")
-def add():
+def add(user: Annotated[User, Depends(access)]):
     return {"message": "Create a credit card"}
 
 
 @router.get("/", description="Get all customer's credit cards")
-def list(user: Annotated[User, Depends(security.get_current_active_user)]):
+def list(user: Annotated[User, Depends(access)]):
     return {"message": "Listing credits cards", "id": user.id}
 
 
 @router.delete("/{id}", description="Delete a customer's credit card by its ID")
-def delete(id: int):
+def delete(id: int, user: Annotated[User, Depends(access)]):
     return {"message": "Deleting a credit card", "id": id}
