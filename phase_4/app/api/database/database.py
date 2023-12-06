a9ae01f06b6269c3a6e307e485b2cd5e9914d352
Diego Rocha Cerqueira <diego93rocha@gmail.com>
2023-12-06 00:22:42 -0300

    comunicação com o banco de dados

---
+++ phase_4/app/api/database/database.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+# Configurações de conexão com o banco de dados
+settings = {
+    'user': 'root',
+    'password': 'abobora',
+    'host': 'localhost',  # Endereço do servidor MySQL
+    'database': 'livrum',  # Nome do banco de dados
+    'raise_on_warnings': True
+}
+def connect():
+    # Estabelecendo a conexão
+    try:
+        conn = mysql.connector.connect(**settings)
+
+        if conn.is_connected():
+            print('Conexão ao banco de dados realizada com sucesso!')
+            cursor = conn.cursor()
+
+            # Exemplo: Executando uma consulta
+            cursor.execute('SELECT VERSION()')
+            version = cursor.fetchone()
+            print(f'Versão do MySQL: {version}')
+
+            # Fechar o cursor e a conexão
+            cursor.close()
+            conn.close()
+            return version
+
+    except mysql.connector.Error as e:
+        print(f'Erro ao conectar ao banco de dados: {e}')
--- phase_4/app/api/main.py
+++ phase_4/app/api/main.py
@@ -1,7 +1,10 @@
+import ujson
+import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import Response
 
 from routers import creditCard, cart, genre, coupon, customer
-
+from database.database import connect
 app = FastAPI()
 
 app.include_router(cart.router)
@@ -9,3 +12,13 @@ app.include_router(coupon.router)
 app.include_router(creditCard.router)
 app.include_router(customer.router)
 app.include_router(genre.router)
+
+
+@app.get("/version")
+async def version_api():
+    versao = connect();
+    print(type(versao))
+    return Response(content=ujson.dumps({"versao do banco de dados": versao[0]}), media_type="application/json")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8010)
