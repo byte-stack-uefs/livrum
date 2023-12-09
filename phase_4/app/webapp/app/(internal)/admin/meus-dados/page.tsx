8900cc62677dd385395859711ab1a967fa059f30
Oguelo <alexjunior2411@gmail.com>
2023-12-08 22:41:22 -0300

    testa ai almir
    cansei

--- phase_4/app/webapp/app/(internal)/admin/meus-dados/page.tsx
+++ phase_4/app/webapp/app/(internal)/admin/meus-dados/page.tsx
@@ -58,163 +58,169 @@ const ClientRegister = () => {
 
     const handlePasswordConfirmChange = (e) => {
         setPasswordConfirm(e.target.value);
-        // Verificar correspondência de senhas quando a senha de confirmação é alterada
+       
         setPasswordsMatch(e.target.value === password);
     };
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    };
 
 
 
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <Grid xs={11} sm={9} md={7} lg={5} margin="auto" >
+                    <Typography variant="h5" fontWeight="semi-bold" style={{ textAlign: "left", color: theme.palette.dark.main, marginTop: "20px", marginLeft: "30px" }}>
+                        Dados administrador
+                    </Typography>
+                    <FormControl fullWidth>
+                        <Grid
+                            container
+                            sx={{
+                                margin: "auto",
+                                padding: 3,
+                                borderBottomRightRadius: 20,
+                                borderBottomLeftRadius: 20,
+                            }}
+                            spacing={2}
+                        >
+                            <Grid xs={7}></Grid>
 
-            <Grid xs={11} sm={9} md={7} lg={5} margin="auto" >
-                <Typography variant="h5" fontWeight="semi-bold" style={{ textAlign: "left", color: theme.palette.dark.main, marginTop: "20px", marginLeft: "30px" }}>
-                    Dados administrador
-                </Typography>
-                <FormControl fullWidth>
-                    <Grid
-                        container
-                        sx={{
-                            margin: "auto",
-                            padding: 3,
-                            borderBottomRightRadius: 20,
-                            borderBottomLeftRadius: 20,
-                        }}
-                        spacing={2}
-                    >
-                        <Grid xs={7}></Grid>
-
-                        <Grid xs={7}>
-                            <TextField
-                                required
-                                fullWidth
-                                value={name}
-                                size="small"
-                                variant="outlined"
-                                label= ''
-                                onChange={(e) => setName(e.target.value)}
-                                placeholder= {name}
-                            />
+                            <Grid xs={7}>
+                                <div><strong>Nome:</strong></div>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    value={name}
+                                    size="small"
+                                    variant="outlined"
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder={name}
+                                />
 
 
 
+                            </Grid>
+
+                            <Grid xs={7}>
+                                <div><strong>Email:</strong></div>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    type="email"
+                                    size="small"
+                                    value={email}
+                                    variant="outlined"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder={email}
+                                />
+                            </Grid>
+                            <Grid xs={12} style={{ borderBottom: '1px solid black', height: '1px' }}>
+
+                            </Grid>
+                            <Grid xs={12}>
+                                <div><strong>Senha Atual:</strong></div>
+                                <TextField
+
+                                    size="small"
+                                    type={showPassword ? 'text' : 'password'}
+                                    value={oldPassword}
+                                    placeholder={oldPassword}
+                                    onChange={(e) => setOldPassword(e.target.value)}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            </Grid>
+                            <Grid xs={6}>
+                                <div><strong>Nova Senha :</strong></div>
+                                <TextField
+                                    size="small"
+
+                                    type={showPassword ? 'text' : 'password'}
+                                    value={password}
+                                    onChange={handlePasswordChange}
+
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            </Grid>
+                            <Grid xs={6}>
+                                <div><strong>Confirmar Senha:</strong></div>
+                                <TextField
+                                    size="small"
+
+                                    type={showPassword ? 'text' : 'password'}
+                                    value={passwordConfirm}
+                                    onChange={handlePasswordConfirmChange}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                    edge="end"
+                                                >
+                                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            </Grid>
+
+
+
+
+                            <Grid xs={12} lg={3}>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    fullWidth
+                                    sx={{
+                                        marginTop: "10px",
+                                        backgroundColor: theme.palette.dark.main,
+                                    }}
+                                >
+                                    Salvar
+                                </Button>
+                            </Grid>
                         </Grid>
+                    </FormControl>
 
-                        <Grid xs={7}>
-                            <TextField
-                                required
-                                fullWidth
-                                type="email"
-                                size="small"
-                                value={email}
-                                label= ""
-                                variant="outlined"
-                                onChange={(e) => setEmail(e.target.value)}
-                                placeholder= {email}
-                            />
-                        </Grid>
-                        <Grid xs={12} style={{ borderBottom: '1px solid black', height: '1px' }}>
-
-                        </Grid>
-                        <Grid xs={12}>
-                            <TextField
-    
-                                size="small"
-                                label="Senha Atual"
-                                type={showPassword ? 'text' : 'password'}
-                                value={oldPassword}
-                                placeholder= {oldPassword}
-                                onChange={(e) => setOldPassword(e.target.value)}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </Grid>
-                        <Grid xs={6}>
-                            <TextField
-                                size="small"
-                                label="Nova senha"
-                                type={showPassword ? 'text' : 'password'}
-                                value={password}
-                                onChange={handlePasswordChange}
-                            
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </Grid>
-                        <Grid xs={6}>
-                            <TextField
-                                size="small"
-                                label="Confirmar senha"
-                                type={showPassword ? 'text' : 'password'}
-                                value={passwordConfirm}
-                                onChange={handlePasswordConfirmChange}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </Grid>
-
-
-
-
-                        <Grid xs={12} lg={3}>
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                                fullWidth
-                                sx={{
-                                    marginTop: "10px",
-                                    backgroundColor: theme.palette.dark.main,
-                                }}
-                            >
-                                Salvar
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </FormControl>
-
-            </Grid>
+                </Grid>
+            </form>
         </>
     );
 };
