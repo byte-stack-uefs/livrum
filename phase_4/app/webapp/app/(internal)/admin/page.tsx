a02b05eebe151bedb7c8f69ef7146ccf6b29a00d
Almir Neto <almirneto338@gmail.com>
2023-12-10 15:05:30 -0300

    Adicionando tabela com o ranking de autores

--- phase_4/app/webapp/app/(internal)/admin/page.tsx
+++ phase_4/app/webapp/app/(internal)/admin/page.tsx
@@ -3,11 +3,11 @@
 import { DateTime } from "luxon";
 import { theme } from "@/app/theme";
 import { LineChart } from "@mui/x-charts";
-import { Card, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import DashboardCard from "@/app/components/DashboardCard";
 import { Book, MonetizationOn, Sell } from '@mui/icons-material';
+import { Box, Card, FormControl, InputLabel, LinearProgress, LinearProgressProps, MenuItem, Select, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
 
 
 export default function Page() {
@@ -15,6 +15,7 @@ export default function Page() {
 
     const [chartWidth, setChartWidth] = useState(0);
     const [data, setData] = useState(null);
+    const [author, setAuthor] = useState(null);
 
     useEffect(() => {
         setChartWidth(document.getElementById('chart-parent')?.clientWidth ?? 500);
@@ -75,6 +76,18 @@ export default function Page() {
             month: 'Julho'
         }
     ]
+
+    const authors = [
+        {
+            id: 5,
+            name: 'Fernando Pessoa',
+            popularity: 40,
+        }, {
+            id: 10,
+            name: 'Almir Neto',
+            popularity: 20,
+        }
+    ];
 
     return (
         <Grid container spacing={2} my={2}>
@@ -140,8 +153,72 @@ export default function Page() {
                 </Card>
             </Grid>
             <Grid xs={12}>
-
+                <Card elevation={0} sx={{ borderRadius: 2 }}>
+                    <Grid container padding={2}>
+                        <Grid xs={9}>
+                            <Typography variant="h5" color="darker.main">
+                                Ranking Autores
+                            </Typography>
+                        </Grid>
+                        <Grid xs={3}>
+                            <FormControl fullWidth>
+                                <InputLabel size="small" id="demo-simple-select-label">Autor</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={author}
+                                    label="Autor"
+                                    onChange={(e) => { setAuthor(e.target.value) }}
+                                    size="small"
+                                >
+                                    <MenuItem value={10}>X</MenuItem>
+                                    <MenuItem value={20}>Y</MenuItem>
+                                    <MenuItem value={30}>Z</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">#</TableCell>
+                                <TableCell align="left">Nome</TableCell>
+                                <TableCell align="left">Popularidade</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {authors.map(e => {
+                                return <TableRow>
+                                    <TableCell align="center">
+                                        {e.id}
+                                    </TableCell>
+                                    <TableCell>
+                                        {e.name}
+                                    </TableCell>
+                                    <TableCell>
+                                        <LinearProgressWithLabel value={e.popularity} />
+                                    </TableCell>
+                                </TableRow>
+                            })}
+                        </TableBody>
+                    </Table>
+                </Card>
             </Grid>
         </Grid>
+    );
+}
+
+function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ width: '100%', mr: 1 }}>
+                <LinearProgress sx={{ height: 8, borderRadius: 5 }} variant="determinate" {...props} />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="primary.main">{`${Math.round(
+                    props.value,
+                )}%`}</Typography>
+            </Box>
+        </Box>
     );
 }
