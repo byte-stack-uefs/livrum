93db0bec8486b4da7768729e077d3b2856ca2ab8
BRCZ1N <brunocamposo.r@gmail.com>
2023-12-11 17:01:12 -0300

    Iniciando/Atualizando - Faltando mascaras e alinhamento a direita do botão

--- phase_4/app/webapp/app/cliente/meus-dados/page.tsx
+++ phase_4/app/webapp/app/cliente/meus-dados/page.tsx
@@ -1,7 +1,181 @@
-export default function Page() {
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import Ebook from "@/app/interfaces/Ebook";
+import Divider from "@/app/components/Divider";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { CloudDownload, CheckCircle, ReportProblem } from "@mui/icons-material";
+import { Box, Button, InputLabel, List, ListItem, Pagination, Stack, TextField, Typography } from "@mui/material";
+import { theme } from "@/app/theme";
+
+
+function ClientDataContainerHeader() {
     return (
-        <div>
-            <p>Essa é a página "Meus dados" que herda o layout de cliente</p>
-        </div>
+        <Grid xs={12}>
+            <Grid xs={12} sx={{ fontSize: 28 }}>
+                <Typography variant="h3">
+                    Meus Dados
+                </Typography>
+            </Grid>
+            <Grid xs={12}>
+                <Divider height={4} width={"10%"} />
+            </Grid>
+        </Grid>
     );
+}
+
+function ClientDataContainer() {
+
+    const [cpf, setCpf] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [password, setPassword] = useState("");
+    const [telephone, setTelephone] = useState("");
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleSubmitClient = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <Grid xs={11} sm={9} md={7} lg={5} style={{ width: '100%' }} >
+            <form onSubmit={handleSubmitClient}>
+                <Grid container
+                    spacing={2}
+                >
+                    <Grid xs={6}>
+                        <InputLabel htmlFor="labelName" required style={{ textAlign: 'left', color: theme.palette.dark.main }}>E-mail </InputLabel>
+                        <TextField
+                            id="labelEmail"
+                            fullWidth
+                            value={name}
+                            size="small"
+                            variant="outlined"
+                            InputProps={{
+                                readOnly: true,
+                                style: { backgroundColor: '#A2B0C1' },
+                            }}
+                        />
+                    </Grid>
+                    <Grid xs={6}>
+                        <InputLabel htmlFor="labelCpf" required style={{ textAlign: 'left', color: theme.palette.dark.main }}>CPF </InputLabel>
+                        <TextField
+                            fullWidth
+                            id="labelCpf"
+                            value={cpf}
+                            size="small"
+                            variant="outlined"
+                            InputProps={{
+                                readOnly: true,
+                                style: { backgroundColor: '#A2B0C1' },
+                            }}
+                        />
+                    </Grid>
+
+                    <Grid xs={6}>
+                        <InputLabel htmlFor="labelBirthday" required style={{ textAlign: 'left', color: theme.palette.dark.main }}> Data de nascimento </InputLabel>
+                        <TextField
+                            fullWidth
+                            id="labelBirthday"
+                            value={birthday}
+                            size="small"
+                            variant="outlined"
+                            InputProps={{
+                                readOnly: true,
+                                style: { backgroundColor: '#A2B0C1' },
+                            }}
+                        />
+                    </Grid>
+
+                    <Grid xs={6}>
+                        <InputLabel htmlFor="labelTelephone" style={{ textAlign: 'left', color: theme.palette.dark.main }}> Telefone </InputLabel>
+                        <TextField
+                            required
+                            fullWidth
+                            id="labelTelephone"
+                            size="small"
+                            value={telephone}
+                            variant="outlined"
+                            onChange={(e) => setTelephone(e.target.value)}
+                            sx={{ backgroundColor: 'white' }}
+                        />
+                    </Grid>
+
+                    <Grid xs={12}>
+                        <InputLabel htmlFor="labelEmail" style={{ textAlign: 'left', color: theme.palette.dark.main }}> E-mail </InputLabel>
+                        <TextField
+                            required
+                            fullWidth
+                            type="email"
+                            size="small"
+                            value={email}
+                            id="labelEmail"
+                            variant="outlined"
+                            onChange={(e) => setEmail(e.target.value)}
+                            sx={{ backgroundColor: 'white' }}
+                        />
+                    </Grid>
+                    <Grid xs={6}>
+                        <InputLabel htmlFor="labelPassword" style={{ textAlign: 'left', color: theme.palette.dark.main }}> Senha </InputLabel>
+                        <TextField
+                            required
+                            fullWidth
+                            size="small"
+                            id="labelPassword"
+                            type="password"
+                            value={password}
+                            variant="outlined"
+                            onChange={(e) => setPassword(e.target.value)}
+                            sx={{ backgroundColor: 'white' }}
+                        />
+                    </Grid>
+                    <Grid xs={12}>
+                        <InputLabel htmlFor="labelAddress" style={{ textAlign: 'left', color: theme.palette.dark.main }}> Endereço </InputLabel>
+                        <TextField
+                            required
+                            fullWidth
+                            type="text"
+                            size="small"
+                            value={address}
+                            id="labelAddress"
+                            variant="outlined"
+                            onChange={(e) => setAddress(e.target.value)}
+                            sx={{ backgroundColor: 'white' }}
+                        />
+                    </Grid>
+                    <Grid xs={12} lg={1} textAlign="right">
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            sx={{
+                                marginTop: "10px",
+                            }}
+                        >
+                            Salvar
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Grid >
+    );
+}
+
+
+export default function Page() {
+    return (<>
+        <Grid container spacing={2}>
+            <Grid xs={12} sx={{ backgroundColor: 'secondary.main', borderRadius: '16px' }}>
+                <ClientDataContainerHeader></ClientDataContainerHeader>
+                <ClientDataContainer></ClientDataContainer>
+            </Grid>
+        </Grid>
+    </>);
 }
