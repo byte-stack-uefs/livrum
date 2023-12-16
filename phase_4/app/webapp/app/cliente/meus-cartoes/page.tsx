4975140aa52ffeeb3fa5930c6effa4a8411ee6ec
Almir Neto <almirneto338@gmail.com>
2023-12-15 23:11:40 -0300

    Ajustando modal de cadastro de cartão

--- phase_4/app/webapp/app/cliente/meus-cartoes/page.tsx
+++ phase_4/app/webapp/app/cliente/meus-cartoes/page.tsx
@@ -1,56 +1,61 @@
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
+"use client";
+
+import { useState } from "react";
 import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography/Typography";
+import { Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 
 export default function Page() {
-    return (
-      <>
-      <Grid container >
-        <Grid xs={11} sm={9} md={7} lg={5} margin="auto">
-          <Card style={{ width: 954, height: 450 }}>
-              <CardContent>
-                <Typography variant="h5" component="div" style={{ textAlign: 'center', fontSize: 23}}>
-                  Cadastro de Cartão de Crédito
-                </Typography>
-                <TextField
-                  label={"Nome no Cartão*"}
-                  fullWidth
-                  size="small"
-                  style={{ margin: '10px 0' }}
-                />
-                <TextField
-                  label={"Numero do Cartão*"}
-                  fullWidth
-                  size="small"
-                  style={{ margin: '10px 0' }}
-                />
-                <TextField
-                  label={"Data de Nascimento*"}
-                  fullWidth
-                  size="small"
-                  style={{ margin: '10px 0' }}
-                />
-                <TextField 
-                  label={"CVV*"}
-                  fullWidth
-                  size="small"
-                  style={{ margin: '10px 0' }} />
+    const [open, setOpen] = useState(false);
 
-                <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
-                  <Button variant="contained" style={{ backgroundColor: '#D95D56', color: '#FFFFFF' }}>
-                    Cancelar
-                  </Button>
-                  <Button variant="contained" style={{ backgroundColor: '#8CD087', color: '#FFFFFF', 'marginLeft': 0 }}>
-                    Salvar
-                  </Button>
-                </div>
-              </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </>
-  );
+    function handleClose() {
+        setOpen(false);
+    }
+
+    return (
+        <>
+            <Grid container>
+                <Button
+                    onClick={() => {
+                        setOpen(true);
+                    }}
+                >
+                    Test
+                </Button>
+            </Grid>
+
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle textAlign="center" sx={{ backgroundColor: "secondary.main", p: 1 }}>
+                    Cadastro novo cartão
+                </DialogTitle>
+                <DialogContent>
+                    <Grid container spacing={2} sx={{ mt: 1 }}>
+                        <Grid item xs={12}>
+                            <TextField label={"Nome no Cartão *"} fullWidth size="small" />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField label={"Numero do Cartão *"} fullWidth size="small" />
+                        </Grid>
+                        <Grid item xs={12} container>
+                            <Grid item xs={6}>
+                                <TextField type="date" label={"Data de Nascimento *"} fullWidth size="small" />
+                            </Grid>
+                            <Grid item xs={6} pl={2}>
+                                <TextField label={"CVV *"} fullWidth size="small" />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Button color="error" variant="contained" onClick={handleClose}>
+                        Cancelar
+                    </Button>
+                    <Button color="success" variant="contained" onClick={handleClose}>
+                        Salvar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
 }
