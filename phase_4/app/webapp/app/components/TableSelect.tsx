b225fbbf8af14985c2cb2d8981648eb3da910e9b
Almir Neto <almirneto338@gmail.com>
2023-12-17 12:10:16 -0300

    Criando um componente para o selected das tabelas internas

---
+++ phase_4/app/webapp/app/components/TableSelect.tsx
@@ -0,0 +1,35 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useState } from "react";
+
+export function TableSelect({ title, onChange, items }: { title: string, items: Array<{ value: string | number, title: string }>, onChange: (value: string | number) => void }) {
+
+    const [selected, setSelected] = useState("");
+
+    return (<FormControl fullWidth>
+        <InputLabel size="small" id={"table-" + title + "-select-label"}>
+            {title}
+        </InputLabel>
+        <Select
+            labelId={"table-" + title + "-select-label"}
+            id={"table-" + title + "-select"}
+            value={selected}
+            label={title}
+            onChange={(e) => {
+                onChange(e.target.value);
+                setSelected(e.target.value);
+            }}
+            size="small"
+            sx={{
+                borderRadius: 3,
+                backgroundColor: "secondary.main",
+                "& > .MuiOutlinedInput-notchedOutline": {
+                    border: "none",
+                },
+            }}
+        >
+            {items.map((e) => {
+                return <MenuItem value={e.value}>{e.title}</MenuItem>
+            })}
+        </Select>
+    </FormControl>);
+}
