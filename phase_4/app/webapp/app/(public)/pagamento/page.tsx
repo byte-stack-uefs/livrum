02ce46cb5603a10ffb58a7ad4698e8029cbdc0ec
Almir Neto <almirneto338@gmail.com>
2023-12-17 16:39:00 -0300

    Movendo código para componente de pix

--- phase_4/app/webapp/app/(public)/pagamento/page.tsx
+++ phase_4/app/webapp/app/(public)/pagamento/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import qrcode from '@/app/assets/qrcode.png'
 import Divider from "@/app/components/Divider";
 import { CreditCard, Pix } from "@mui/icons-material";
 import { TabSelector } from "@/app/components/TabSelector";
@@ -176,19 +175,7 @@ export default function Page() {
     );
 }
 
-function PixContainer() {
-    return (
-        <Grid container>
-            <Grid xs={12} textAlign="center">
-                <Image width={400} height={400} src={qrcode} alt="PaymentQrCode" />
-            </Grid>
-            <Grid xs={3}></Grid>
-            <Grid xs={12} md={6}>
-                <p style={{ textAlign: 'center' }}>Escaneie o QRCode e realize o pagamento no aplicativo do banco</p>
-            </Grid>
-        </Grid>
-    );
-}
+
 
 function CreditCardContainer() {
 
