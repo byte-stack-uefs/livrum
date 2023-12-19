984957bf35c1af76fc5ddd9b8803d1e63adbdeab
Almir Neto <almirneto338@gmail.com>
2023-12-18 22:37:37 -0300

    Permitindo requisição de localhost para api

--- phase_4/app/api/main.py
+++ phase_4/app/api/main.py
@@ -4,12 +4,23 @@ from fastapi import FastAPI
 from dependencies import security
 from database.database import connect
 from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
 from routers import creditCard, cart, genre, coupon, customer
 import logging
 
 logging.getLogger("passlib").setLevel(logging.ERROR)
 
 app = FastAPI()
+
+origins = ["http://localhost"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(security.router)
 app.include_router(cart.router)
