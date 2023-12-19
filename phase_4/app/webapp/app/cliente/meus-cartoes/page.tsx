8a753b33af1b7e1679516fc78973da85b4fa93c6
Almir Neto <almirneto338@gmail.com>
2023-12-19 19:50:57 -0300

    Ajustes visuais

--- phase_4/app/webapp/app/cliente/meus-cartoes/page.tsx
+++ phase_4/app/webapp/app/cliente/meus-cartoes/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { theme } from "@/app/theme";
 import { useState } from "react";
-import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
+import { Add } from "@mui/icons-material";
+import Divider from "@/app/components/Divider";
 import TextField from "@mui/material/TextField";
-import { Box, Card, CardActions, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, Icon, List, ListItem, Paper, Stack, Typography } from "@mui/material";
-import CreditCardIcon from '@mui/icons-material/CreditCard';
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import { Box, Card, CardActions, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, List, ListItem, Typography } from "@mui/material";
 
 export default function Page() {
     const [open, setOpen] = useState(false);
@@ -16,20 +17,21 @@ export default function Page() {
         {
             id: 0,
             num: "1234123412341234",
-            expiryDate: "27/11/2030",
+            expiryDate: "11/2030",
             cvc: 110,
             name: "Alguem da Silva",
-        },{
+        },
+        {
             id: 1,
             num: "4321432143214321",
-            expiryDate: "27/11/2040",
+            expiryDate: "11/2040",
             cvc: 852,
             name: "Alguem dos Santos",
         },
         {
             id: 2,
             num: "4567456745674567",
-            expiryDate: "27/11/2035",
+            expiryDate: "11/2035",
             cvc: 951,
             name: "Alguem de Jesus",
         },
@@ -40,65 +42,72 @@ export default function Page() {
     }
 
     return (
-        <>
-     
-        <Grid xs={12} sx={{ fontSize: 18 }}>
-            <Typography variant="h4" fontWeight="bold" color="primary">
-                Meus Cartões
-            </Typography>
-        </Grid>
- 
-        <Grid xs={12} md={6} >
-            <Grid item xs={12} sx={{ backgroundColor: "secondary.main", p: 1, margin: 'auto', borderBottomRightRadius: 20, }}>
-                <Grid justifyContent="flex-end" alignItems={"flex-end"}>
-                    <Button variant="contained"
-                        onClick={() => {
-                            setOpen(true);
-                        }}
-                    >
-                        Adicionar
-                    </Button>
+        <Box sx={{ backgroundColor: "secondary.main", borderRadius: 5 }}>
+            <Grid container>
+                <Grid xs={12} p={2}>
+                    <Typography variant="h4" fontWeight="bold" color="darker.main">
+                        Meus Cartões
+                    </Typography>
+                    <Divider width="15%" />
                 </Grid>
-                
-                <List sx={{ width: "100%", height: "100%"}} >
-                {creditCards.map((creditcard) => (
-                    <ListItem key={creditcard.id}> 
-                        <Grid sx={{ backgroundColor: "white", margin: 'auto', minWidth: 350}}>
-                            <CardContent>
-                                <Typography variant="body1">{creditcard.name}</Typography>
-                                <Typography variant="body2">Validade: {creditcard.expiryDate}</Typography>
-                                <Typography>{creditcard.num}</Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color='error'>Delete</Button>
-                            </CardActions>
-                        </Grid>
-                       
-                    </ListItem>
+                <Grid xs={12} p={2}>
+                    <Box sx={{ backgroundColor: "#FFF", borderRadius: 5 }}>
+                        <Grid container p={2}>
+                            <Grid xs={12} textAlign="right">
+                                <Button
+                                    startIcon={<Add />}
+                                    variant="contained"
+                                    onClick={() => {
+                                        setOpen(true);
+                                    }}
+                                >
+                                    Adicionar
+                                </Button>
+                            </Grid>
 
-                ))}
-            
-                </List>  
-            </Grid> 
-        </Grid>
-        
+                            <Grid xs={12}>
+                                <List sx={{ width: "100%", height: "100%" }}>
+                                    {creditCards.map((creditcard) => (
+                                        <ListItem key={creditcard.id}>
+                                            <Grid sx={{ backgroundColor: "white", margin: "auto", minWidth: 350 }}>
+                                                <CardContent>
+                                                    <CreditCardIcon />
+                                                    <Typography variant="body1">{creditcard.name}</Typography>
+                                                    <Typography variant="body2">Válido até {creditcard.expiryDate}</Typography>
+                                                    <Typography>{creditcard.num}</Typography>
+                                                </CardContent>
+                                                <CardActions>
+                                                    <Button size="small" color="error">
+                                                        Delete
+                                                    </Button>
+                                                </CardActions>
+                                            </Grid>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Grid>
+            </Grid>
+
             <Dialog open={open} onClose={handleClose}>
                 <DialogTitle textAlign="center" sx={{ backgroundColor: "secondary.main", p: 1 }}>
                     Cadastro novo cartão
                 </DialogTitle>
                 <DialogContent>
                     <Grid container spacing={2} sx={{ mt: 1 }}>
-                        <Grid item xs={12}>
+                        <Grid xs={12}>
                             <TextField label={"Nome no Cartão *"} fullWidth size="small" />
                         </Grid>
-                        <Grid item xs={12}>
+                        <Grid xs={12}>
                             <TextField label={"Numero do Cartão *"} fullWidth size="small" />
                         </Grid>
-                        <Grid item xs={12} container>
-                            <Grid item xs={6}>
+                        <Grid xs={12} container>
+                            <Grid xs={6}>
                                 <TextField type="date" label={"Data de Nascimento *"} fullWidth size="small" />
                             </Grid>
-                            <Grid item xs={6} pl={2}>
+                            <Grid xs={6} pl={2}>
                                 <TextField label={"CVV *"} fullWidth size="small" />
                             </Grid>
                         </Grid>
@@ -113,6 +122,6 @@ export default function Page() {
                     </Button>
                 </DialogActions>
             </Dialog>
-        </>
+        </Box>
     );
 }
