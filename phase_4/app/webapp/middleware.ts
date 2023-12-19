2fdbb69430e800c9016d6590bdcd6f10345169c8
Almir Neto <almirneto338@gmail.com>
2023-12-19 17:43:05 -0300

    Desabilitando login momentâneamente

--- phase_4/app/webapp/middleware.ts
+++ phase_4/app/webapp/middleware.ts
@@ -1,34 +1,17 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-
-    if (request.nextUrl.pathname == '/') {
+export function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname == "/") {
         return;
     }
 
-    console.log(request.cookies.has('token'))
-
-    if (request.cookies.has('token')) {
-
-        const cookie = request.cookies.get('token')?.value;
-        console.log(cookie)
-        const headers = new Headers();
-
-        headers.append('Authorization', 'Bearer ' + cookie);
-        const response = await fetch('http://localhost:3000/account/isAuthenticated', {
-            headers: headers,
-            cache: 'no-store',
-            next: {
-                revalidate: 0
-            }
-        });
-        let data = await response.text()
-        console.debug(data);
+    if (request.cookies.has("token")) {
+        return;
     }
 
-    return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
@@ -40,6 +23,6 @@ export const config = {
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
          */
-        '/((?!api|login|admin/login|recuperacao-senha|catalogo|_next/static|_next/image|favicon.ico).*)',
+        "/((?!api|login|admin/login|recuperacao-senha|catalogo|_next/static|_next/image|favicon.ico).*)",
     ],
-}
+};
