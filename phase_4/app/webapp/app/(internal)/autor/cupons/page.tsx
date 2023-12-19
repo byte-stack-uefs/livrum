b897000e30d21a7bb3bb7c5e5b76967fb4f8d505
Almir Neto <almirneto338@gmail.com>
2023-12-19 17:51:57 -0300

    Ajustando grid

--- phase_4/app/webapp/app/(internal)/autor/cupons/page.tsx
+++ phase_4/app/webapp/app/(internal)/autor/cupons/page.tsx
@@ -56,160 +56,155 @@ const autorCupons = () => {
     };
 
     return (
-        <main>
-            <Grid>
-                <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{ backgroundColor: "" }}>
-                    <Grid sx={{ height: 100 }}>
-                        <Grid xs={4}>
-                            <Typography sx={{ color: theme.palette.dark.main }} variant="h6" paddingTop={2}>
-                                Meus cupons
-                            </Typography>
-                            <Divider width={"50%"} />
-                        </Grid>
+        <Grid container>
+            <Grid xs={12} container direction="row" justifyContent="space-between" alignItems="center" mb={2}>
+                <Grid xs={4}>
+                    <Typography sx={{ color: theme.palette.dark.main }} variant="h6">
+                        Meus cupons
+                    </Typography>
+                    <Divider width={"35%"} />
+                </Grid>
+                <Grid xs={3}>
+                    <Button
+                        variant="contained"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setOpenModal(true);
+                        }}
+                    >
+                        Criar novo cupom
+                    </Button>
+                </Grid>
+            </Grid>
+
+            <Grid container xs={12} display="flex" justifyContent="center" alignItems="center">
+                <Grid xs={12} sx={{ backgroundColor: "#FFFFFF", borderRadius: 4 }}>
+                    <Grid container direction="row" justifyContent="space-around" alignItems="center" paddingTop={1}>
+                        <Typography color={"black"} sx={{ width: 78, textAlign: "center" }}>
+                            Percentual
+                        </Typography>
+                        <Typography color={"black"} sx={{ width: 85, textAlign: "center" }}>
+                            Nome
+                        </Typography>
+                        <Typography color={"black"} sx={{ width: 58, textAlign: "center" }}>
+                            Status
+                        </Typography>
+                        <Typography color={"black"} sx={{ width: 73, textAlign: "center" }}>
+                            Excluir
+                        </Typography>
                     </Grid>
 
-                    <Grid xs={3} paddingTop={2}>
+                    <Grid paddingTop={1}>
+                        <Divider width={"100%"} />
+                    </Grid>
+
+                    <Grid container direction="row" justifyContent="space-around" alignItems="center" paddingTop={1}>
+                        <Grid width={78} display="flex" direction="row" justifyContent="space-around">
+                            <Grid
+                                display="flex"
+                                justifyContent="center"
+                                alignItems="center"
+                                sx={{
+                                    boxShadow: "rgba(0, 0, 0, 0.24) 2px 2px 5px",
+                                    backgroundColor: "#D9D9D9",
+                                    borderRadius: "50%",
+                                    width: 40,
+                                    height: 40,
+                                }}
+                            >
+                                <Typography fontSize={18} color={"black"}>
+                                    50%
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Typography fontSize={12}>"NOMECUPOM"</Typography>
+                        <Switch></Switch>
+                        <Button variant="contained" color="error" sx={{ fontSize: 10 }}>
+                            Excluir
+                        </Button>
+                    </Grid>
+
+                    <Grid display="flex" justifyContent="center" alignItems="center" paddingTop={1}>
+                        <Divider height={1} width={"90%"} />
+                    </Grid>
+
+                    <Grid container direction="row" justifyContent="space-around" alignItems="center" paddingTop={1}>
+                        <Grid width={78} display="flex" direction="row" justifyContent="space-around">
+                            <Grid
+                                display="flex"
+                                justifyContent="center"
+                                alignItems="center"
+                                sx={{
+                                    boxShadow: "rgba(0, 0, 0, 0.24) 2px 2px 5px",
+                                    backgroundColor: "lightgrey",
+                                    borderRadius: "50%",
+                                    width: 40,
+                                    height: 40,
+                                }}
+                            >
+                                <Typography fontSize={18} color={"black"}>
+                                    50%
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Typography fontSize={12}>"NOMECUPOM"</Typography>
+                        <Switch></Switch>
+                        <Grid width={73} display="flex" direction="row" justifyContent="space-around">
+                            <Button variant="contained" color="error">
+                                <DeleteOutlinedIcon sx={{ fontSize: 16 }} width={73} />
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            <BootstrapDialog open={openModal} onClose={handleClose} fullWidth maxWidth="sm">
+                <StyledDialogTitle id="customized-dialog-title">
+                    <strong>Criar novo cupom</strong>
+                </StyledDialogTitle>
+                <DialogContent dividers>
+                    <Grid container direction="row" justifyContent="center" alignItems="center">
+                        <Typography>Nome do cupom: </Typography>
+                        <TextField size="small"></TextField>
+                    </Grid>
+                    <Grid container direction="row" justifyContent="center" alignItems="center">
+                        <Typography>Percentual: </Typography>
+                        <Select onChange={handleChange}>
+                            <MenuItem value={10}>10%</MenuItem>
+                            <MenuItem value={20}>20%</MenuItem>
+                            <MenuItem value={30}>30%</MenuItem>
+                            <MenuItem value={40}>40%</MenuItem>
+                            <MenuItem value={50}>50%</MenuItem>
+                        </Select>
+                    </Grid>
+                </DialogContent>
+
+                <DialogActions>
+                    <Stack justifyContent="center" gap={2} flexDirection="row" width={1.0} flexWrap="wrap">
                         <Button
+                            color="success"
                             variant="contained"
                             onClick={(e) => {
                                 e.preventDefault();
-                                setOpenModal(true);
+                                setOpenModal(false);
                             }}
                         >
-                            Criar novo cupom
+                            Criar
                         </Button>
-                    </Grid>
-                </Grid>
-
-                <Grid display="flex" justifyContent="center" alignItems="center">
-                    <Grid sx={{ backgroundColor: "#FFFFFF", borderRadius: 10, height: 400, width: 700 }}>
-                        <Grid container direction="row" justifyContent="space-around" alignItems="center" paddingTop={1}>
-                            <Typography color={"black"} sx={{ width: 78, textAlign: "center" }}>
-                                Percentual
-                            </Typography>
-                            <Typography color={"black"} sx={{ width: 85, textAlign: "center" }}>
-                                Nome
-                            </Typography>
-                            <Typography color={"black"} sx={{ width: 58, textAlign: "center" }}>
-                                Status
-                            </Typography>
-                            <Typography color={"black"} sx={{ width: 73, textAlign: "center" }}>
-                                Excluir
-                            </Typography>
-                        </Grid>
-
-                        <Grid paddingTop={1}>
-                            <Divider width={"100%"} />
-                        </Grid>
-
-                        <Grid container direction="row" justifyContent="space-around" alignItems="center" paddingTop={1}>
-                            <Grid width={78} display="flex" direction="row" justifyContent="space-around">
-                                <Grid
-                                    display="flex"
-                                    justifyContent="center"
-                                    alignItems="center"
-                                    sx={{
-                                        boxShadow: "rgba(0, 0, 0, 0.24) 2px 2px 5px",
-                                        backgroundColor: "#D9D9D9",
-                                        borderRadius: "50%",
-                                        width: 40,
-                                        height: 40,
-                                    }}
-                                >
-                                    <Typography fontSize={18} color={"black"}>
-                                        50%
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                            <Typography fontSize={12}>"NOMECUPOM"</Typography>
-                            <Switch></Switch>
-                            <Button variant="contained" color="error" sx={{ fontSize: 10 }}>
-                                Excluir
-                            </Button>
-                        </Grid>
-
-                        <Grid display="flex" justifyContent="center" alignItems="center" paddingTop={1}>
-                            <Divider height={1} width={"90%"} />
-                        </Grid>
-
-                        <Grid container direction="row" justifyContent="space-around" alignItems="center" paddingTop={1}>
-                            <Grid width={78} display="flex" direction="row" justifyContent="space-around">
-                                <Grid
-                                    display="flex"
-                                    justifyContent="center"
-                                    alignItems="center"
-                                    sx={{
-                                        boxShadow: "rgba(0, 0, 0, 0.24) 2px 2px 5px",
-                                        backgroundColor: "lightgrey",
-                                        borderRadius: "50%",
-                                        width: 40,
-                                        height: 40,
-                                    }}
-                                >
-                                    <Typography fontSize={18} color={"black"}>
-                                        50%
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                            <Typography fontSize={12}>"NOMECUPOM"</Typography>
-                            <Switch></Switch>
-                            <Grid width={73} display="flex" direction="row" justifyContent="space-around">
-                                <Button variant="contained" color="error">
-                                    <DeleteOutlinedIcon sx={{ fontSize: 16 }} width={73} />
-                                </Button>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-
-                <BootstrapDialog open={openModal} onClose={handleClose} fullWidth maxWidth="sm">
-                    <StyledDialogTitle id="customized-dialog-title">
-                        <strong>Criar novo cupom</strong>
-                    </StyledDialogTitle>
-                    <DialogContent dividers>
-                        <Grid container direction="row" justifyContent="center" alignItems="center">
-                            <Typography>Nome do cupom: </Typography>
-                            <TextField size="small"></TextField>
-                        </Grid>
-                        <Grid container direction="row" justifyContent="center" alignItems="center">
-                            <Typography>Percentual: </Typography>
-                            <Select onChange={handleChange}>
-                                <MenuItem value={10}>10%</MenuItem>
-                                <MenuItem value={20}>20%</MenuItem>
-                                <MenuItem value={30}>30%</MenuItem>
-                                <MenuItem value={40}>40%</MenuItem>
-                                <MenuItem value={50}>50%</MenuItem>
-                            </Select>
-                        </Grid>
-                    </DialogContent>
-
-                    <DialogActions>
-                        <Stack justifyContent="center" gap={2} flexDirection="row" width={1.0} flexWrap="wrap">
-                            <Button
-                                color="success"
-                                variant="contained"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    setOpenModal(false);
-                                }}
-                            >
-                                Criar
-                            </Button>
-                            <Button
-                                color="error"
-                                variant="contained"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    setOpenModal(false);
-                                }}
-                            >
-                                Cancelar
-                            </Button>
-                        </Stack>
-                    </DialogActions>
-                </BootstrapDialog>
-            </Grid>
-        </main>
+                        <Button
+                            color="error"
+                            variant="contained"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setOpenModal(false);
+                            }}
+                        >
+                            Cancelar
+                        </Button>
+                    </Stack>
+                </DialogActions>
+            </BootstrapDialog>
+        </Grid>
     );
 };
 
