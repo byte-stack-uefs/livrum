7bdd5e53403b5a400534626ea4d1882912ed7dea
Almir Neto <almirneto338@gmail.com>
2023-12-20 10:17:50 -0300

    Format

--- phase_4/app/webapp/app/(public)/ebook/[id]/page.tsx
+++ phase_4/app/webapp/app/(public)/ebook/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { theme } from "@/app/theme";
@@ -11,61 +11,58 @@ import { Button, Container, Tooltip, Typography } from "@mui/material";
 import Ebook from "@/app/interfaces/Ebook";
 import { useCart } from "../../carrinho/useCart";
 
-
 interface EbookPageParams {
     id: number;
 }
 
-interface EbookDetailsProps{
-    ebook: any
+interface EbookDetailsProps {
+    ebook: any;
 }
 
 export type CartItemType = {
-    id: number,
-    title: string,
-    cover: string,
-    author: string,
-    price: number,
-}
-
+    id: number;
+    title: string;
+    cover: string;
+    author: string;
+    price: number;
+};
 
 export default function Page({ params }: { params: EbookPageParams }) {
-   
     const [height, setHeight] = useState(0);
     const [width, setWidth] = useState(0);
 
     useEffect(() => {
-        const el = document.querySelector('#ebook-cover-container');
-        setWidth(el?.clientWidth ?? 0)
-        setHeight(el?.clientHeight ?? 0)
+        const el = document.querySelector("#ebook-cover-container");
+        setWidth(el?.clientWidth ?? 0);
+        setHeight(el?.clientHeight ?? 0);
     }, []);
 
     const similars = [
         {
-            title: 'AAA',
-            author: 'Almir',
-            cover: 'https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg'
+            title: "AAA",
+            author: "Almir",
+            cover: "https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg",
         },
         {
-            title: 'AAA',
-            author: 'Almir',
-            cover: 'https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg'
+            title: "AAA",
+            author: "Almir",
+            cover: "https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg",
         },
         {
-            title: 'AAA',
-            author: 'Almir',
-            cover: 'https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg'
+            title: "AAA",
+            author: "Almir",
+            cover: "https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg",
         },
         {
-            title: 'AAA',
-            author: 'Almir',
-            cover: 'https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg'
+            title: "AAA",
+            author: "Almir",
+            cover: "https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg",
         },
         {
-            title: 'AAA',
-            author: 'Almir',
-            cover: 'https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg'
-        }
+            title: "AAA",
+            author: "Almir",
+            cover: "https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg",
+        },
     ];
 
     const ebook = {
@@ -74,37 +71,33 @@ export default function Page({ params }: { params: EbookPageParams }) {
         cover: "https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg",
         author: "Fiodor Dostoievsk",
         price: 110,
-    }
-    const {handleAddEbookToCart} = useCart()
-
-    function checkIsProductInCart(item: CartItemType){
-       
-       if(useCart().cartItems){
-        const existingIndex = useCart().cartItems.findIndex(
-            (product) => product.id == item.id
-        );
-
-        if(existingIndex > -1){
-            return true;
-        }else{
-            return false;
-        }
-       }
     };
+    const { handleAddEbookToCart } = useCart();
 
-    function mensagemClick(){
-        <span>Produto adicionado ao carrinho</span>
+    function checkIsProductInCart(item: CartItemType) {
+        if (useCart().cartItems) {
+            const existingIndex = useCart().cartItems.findIndex((product) => product.id == item.id);
+
+            if (existingIndex > -1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
-    function handleClickAddCart(item: CartItemType){
+
+    function mensagemClick() {
+        <span>Produto adicionado ao carrinho</span>;
+    }
+    function handleClickAddCart(item: CartItemType) {
         handleAddEbookToCart(ebook);
         mensagemClick();
     }
 
     return (
         <Container maxWidth={false}>
-
             <Grid container>
-                <Grid py={2} px={4} container xs={12} sx={{ backgroundColor: 'secondary.main', borderRadius: 5, marginY: 6 }}>
+                <Grid py={2} px={4} container xs={12} sx={{ backgroundColor: "secondary.main", borderRadius: 5, marginY: 6 }}>
                     <Grid xs={12} textAlign="center" py={2}>
                         <Typography variant="h4" color="dark.main">
                             TÍTULO DO EBOOK
@@ -112,7 +105,13 @@ export default function Page({ params }: { params: EbookPageParams }) {
                     </Grid>
                     <Grid container xs={12} py={2}>
                         <Grid xs={5} id="ebook-cover-container" p={2}>
-                            <Image src="https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg" width={width} height={height} alt="Ebook cover" style={{ height: '100%', width: '100%', borderRadius: 30, objectFit: 'contain' }} />
+                            <Image
+                                src="https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg"
+                                width={width}
+                                height={height}
+                                alt="Ebook cover"
+                                style={{ height: "100%", width: "100%", borderRadius: 30, objectFit: "contain" }}
+                            />
                         </Grid>
                         <Grid container xs={7} p={2}>
                             <Grid xs={12} pt={6}>
@@ -120,37 +119,42 @@ export default function Page({ params }: { params: EbookPageParams }) {
                                     Descrição
                                 </Typography>
                             </Grid>
-                            <Grid xs={12} sx={{ color: 'textLight.main' }}>
+                            <Grid xs={12} sx={{ color: "textLight.main" }}>
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus
-                                    recusandae facere eveniet, magni tempora praesentium alias itaque explicabo accusamus
-                                    asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint consequatur?
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus recusandae facere eveniet, magni tempora
+                                    praesentium alias itaque explicabo accusamus asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint
+                                    consequatur?
                                 </p>
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus
-                                    recusandae facere eveniet, magni tempora praesentium alias itaque explicabo accusamus
-                                    asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint consequatur?
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus recusandae facere eveniet, magni tempora
+                                    praesentium alias itaque explicabo accusamus asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint
+                                    consequatur?
                                 </p>
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus
-                                    recusandae facere eveniet, magni tempora praesentium alias itaque explicabo accusamus
-                                    asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint consequatur?
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus recusandae facere eveniet, magni tempora
+                                    praesentium alias itaque explicabo accusamus asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint
+                                    consequatur?
                                 </p>
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus
-                                    recusandae facere eveniet, magni tempora praesentium alias itaque explicabo accusamus
-                                    asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint consequatur?
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus recusandae facere eveniet, magni tempora
+                                    praesentium alias itaque explicabo accusamus asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint
+                                    consequatur?
                                 </p>
                             </Grid>
                             <Grid xs={12} container justifyContent="center" mt={4}>
-                                <Grid container py={3} xs={8} sx={{ backgroundColor: '#c5c5c5', borderRadius: 3 }} textAlign="center" alignSelf="end">
+                                <Grid container py={3} xs={8} sx={{ backgroundColor: "#c5c5c5", borderRadius: 3 }} textAlign="center" alignSelf="end">
                                     <Grid xs={6} alignSelf="center">
                                         <Typography variant="h4" textAlign="center" color="dark.main">
                                             R$ 39,90
                                         </Typography>
                                     </Grid>
                                     <Grid xs={6} alignSelf="center">
-                                        <Button disabled={checkIsProductInCart(ebook)} variant="contained" onClick={() => handleClickAddCart(ebook)} startIcon={<AddShoppingCart />}>
+                                        <Button
+                                            disabled={checkIsProductInCart(ebook)}
+                                            variant="contained"
+                                            onClick={() => handleClickAddCart(ebook)}
+                                            startIcon={<AddShoppingCart />}
+                                        >
                                             Comprar
                                         </Button>
                                     </Grid>
@@ -166,7 +170,7 @@ export default function Page({ params }: { params: EbookPageParams }) {
                         </Grid>
                         <Grid xs={12} container>
                             <Grid xs={3}>
-                                <ul style={{ listStyle: 'none', padding: 0 }}>
+                                <ul style={{ listStyle: "none", padding: 0 }}>
                                     <li style={{ paddingTop: 4, paddingBottom: 4 }}>
                                         <ListItem title="Autor" value="X" />
                                     </li>
@@ -179,7 +183,7 @@ export default function Page({ params }: { params: EbookPageParams }) {
                                 </ul>
                             </Grid>
                             <Grid xs={3}>
-                                <ul style={{ listStyle: 'none', padding: 0 }}>
+                                <ul style={{ listStyle: "none", padding: 0 }}>
                                     <li style={{ paddingTop: 4, paddingBottom: 4 }}>
                                         <ListItem title="Idioma" value="Português" />
                                     </li>
@@ -199,7 +203,7 @@ export default function Page({ params }: { params: EbookPageParams }) {
                         <Typography variant="h4" color="dark.main">
                             Títulos semelhantes
                         </Typography>
-                        <Divider width={"15%"} style={{ margin: 'auto' }} />
+                        <Divider width={"15%"} style={{ margin: "auto" }} />
                     </Grid>
                     <Grid xs={12} my={3}>
                         <Carousel items={similars} Child={SimilarEbooks} />
@@ -211,22 +215,22 @@ export default function Page({ params }: { params: EbookPageParams }) {
 }
 
 function SimilarEbooks(ebook: any) {
-    return (<div style={{ textAlign: 'center', padding: 8 }}>
-        <Image src={ebook.cover} width={3000} height={3000} style={{ width: '100%', height: '100%', objectFit: 'contain' }} alt="book cover" />
-        <Typography fontWeight="bold" color="dark.main">
-            {ebook.title}
-        </Typography>
-        <Typography color="dark.main">
-            {ebook.author}
-        </Typography>
-    </div>);
+    return (
+        <div style={{ textAlign: "center", padding: 8 }}>
+            <Image src={ebook.cover} width={3000} height={3000} style={{ width: "100%", height: "100%", objectFit: "contain" }} alt="book cover" />
+            <Typography fontWeight="bold" color="dark.main">
+                {ebook.title}
+            </Typography>
+            <Typography color="dark.main">{ebook.author}</Typography>
+        </div>
+    );
 }
 
-function ListItem({ title, value }: { title: string, value: string }) {
+function ListItem({ title, value }: { title: string; value: string }) {
     return (
         <span>
-            <span style={{ color: theme.palette.dark.main, fontWeight: 'bold' }}>{title}: </span>
-            <span style={{ color: theme.palette.textLight.main, fontWeight: 'bold' }}>{value}</span>
+            <span style={{ color: theme.palette.dark.main, fontWeight: "bold" }}>{title}: </span>
+            <span style={{ color: theme.palette.textLight.main, fontWeight: "bold" }}>{value}</span>
         </span>
     );
 }
