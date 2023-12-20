3bbdf549e96bc22762f84aca43f6d389aa31e2e4
Almir Neto <almirneto338@gmail.com>
2023-12-20 09:56:57 -0300

    Melhorias na página de submissão de ebook

--- phase_4/app/webapp/app/(internal)/autor/ebook/submissao/page.tsx
+++ phase_4/app/webapp/app/(internal)/autor/ebook/submissao/page.tsx
@@ -1,7 +1,31 @@
 "use client";
-import { Button } from "@mui/material";
 import { Box } from "@mui/material";
 import React, { useState } from "react";
+import { Button, Grid, TextField, Typography, styled } from "@mui/material";
+import { Image } from "@mui/icons-material";
+
+const DashedInput = styled(TextField)(({ theme }) => ({
+    borderRadius: 0,
+    border: "none",
+
+    "& .MuiInputBase-root": {
+        backgroundColor: "transparent",
+        borderRadius: 0,
+        border: "1px dashed white",
+        borderColor: theme.palette.dark.main,
+
+        ":hover": {
+            borderColor: theme.palette.darker.main,
+        },
+    },
+    "& .MuiOutlinedInput-notchedOutline": {
+        border: 0,
+    },
+
+    " & .MuiInputBase-inputSizeSmall": {
+        height: "0.7em",
+    },
+}));
 
 function CampoTexto({ texto }) {
     return (
@@ -71,249 +95,123 @@ export default function Page() {
     }
 
     return (
-        <Box height="80vh" display="flex" flexDirection="column" justifyContent="flex-start">
-            {/*Título*/}
-            <input
-                onChange={handleTituloInputChange}
-                type="text"
-                placeholder="Título do seu livro..."
-                style={{
-                    border: "1px dotted #163760",
-                    borderRadius: "4px",
-                    backgroundColor: "#F1F1F1",
-                    borderWidth: "2px",
-                    outline: "none",
-                    width: "300px",
-                    height: "100px",
-                    fontSize: "20px",
-                    color: "#163760",
-                    marginLeft: "250px",
-                    marginTop: "20px",
-                }}
-            />
-
-            {/*Sinopse + imagem*/}
-            <Box display="flex" flexDirection="row">
-                <Box flexDirection="column" display="flex" marginTop="20px">
-                    <label
-                        class="picture"
-                        tabIndex="0"
-                        style={{
-                            display: "flex",
-                            width: "200px",
-                            height: "200px",
-                            background: "#ccc",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            color: "#aaa",
-                            cursor: "pointer",
-                            border: "2px dashed currentcolor",
-                        }}
-                    >
-                        <input onChange={handleImgInputChange} class="picture__input" type="file" accept="image/*" style={{ display: "none" }} />
-                        <span class="picture__image">Escolha uma Imagem</span>
-                    </label>
-                    <label
-                        style={{
-                            marginTop: "20px",
-                            color: "#163760",
-                            fontFamily: "Source Sans Pro, sans-serif",
-                            fontWeight: "bold",
-                            fontSize: "20px",
-                        }}
-                    >
-                        Especificações
-                    </label>
-                </Box>
-
-                <textarea
-                    onChange={handleSinopseInputChange}
-                    type="text"
-                    placeholder="Sinopse do seu livro"
-                    style={{
-                        resize: "none",
-                        marginLeft: "180px",
-                        marginTop: "20px",
-                        border: "1px dotted #163760",
-                        borderRadius: "4px",
-                        backgroundColor: "#F1F1F1",
-                        borderWidth: "2px",
-                        outline: "none",
-                        width: "300px",
-                        height: "200px",
-                        fontSize: "16px",
-                        color: "#163760",
-                    }}
-                />
-            </Box>
-
-            {/*Especificações*/}
-            <Box width="auto" height="65vh" display="flex" flexDirection="row" justifyContent="flex-start">
-                <Box
-                    width="flex"
-                    height="30vh"
-                    display="flex"
-                    flexDirection="column"
-                    justifyContent="flex-start"
-                    marginRight="100px"
-                    marginTop="10px"
-                >
-                    <Box display="flex" alignItems="center" marginBottom="7px">
-                        <label style={{ marginRight: "30px", color: "#163760", fontFamily: "Source Sans Pro, sans-serif", fontWeight: "bold" }}>
-                            Autor:
-                        </label>
-                        <input
-                            onChange={handleAutorInputChange}
-                            type="text"
-                            style={{
-                                border: "1px dotted #163760",
-                                borderRadius: "4px",
-                                backgroundColor: "#F1F1F1",
-                                borderWidth: "2px",
-                                width: "100px",
-                                outline: "none",
-                            }}
-                        />
-                    </Box>
-
-                    <Box display="flex" alignItems="center" marginBottom="7px">
-                        <label style={{ marginRight: "30px", color: "#163760", fontFamily: "Source Sans Pro, sans-serif", fontWeight: "bold" }}>
-                            Número de páginas:
-                        </label>
-                        <input
-                            onChange={handlePagInputChange}
-                            type="text"
-                            style={{
-                                border: "1px dotted #163760",
-                                borderRadius: "4px",
-                                backgroundColor: "#F1F1F1",
-                                borderWidth: "2px",
-                                width: "100px",
-                                outline: "none",
-                            }}
-                        />
-                    </Box>
-
-                    <Box display="flex" alignItems="center" marginBottom="7px">
-                        <label style={{ marginRight: "30px", color: "#163760", fontFamily: "Source Sans Pro, sans-serif", fontWeight: "bold" }}>
-                            Ano de lançamento:
-                        </label>
-                        <input
-                            onChange={handleAnoInputChange}
-                            type="text"
-                            style={{
-                                border: "1px dotted #163760",
-                                borderRadius: "4px",
-                                backgroundColor: "#F1F1F1",
-                                borderWidth: "2px",
-                                width: "100px",
-                                outline: "none",
-                            }}
-                        />
-                    </Box>
-
-                    <Box display="flex" alignItems="center" marginBottom="7px" marginTop="20px">
-                        <label
-                            style={{
-                                marginRight: "14px",
-                                color: "#163760",
-                                fontFamily: "Source Sans Pro, sans-serif",
-                                fontWeight: "bold",
-                                fontSize: "20px",
+        <Grid container>
+            <Grid container justifyContent="center">
+                <Grid xs={12} md={6} item>
+                    <DashedInput fullWidth value={titulo} onChange={handleTituloInputChange} placeholder="Título do seu livro" />
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid xs={12} item container my={3}>
+                    <Grid xs={5} item>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                backgroundColor: "secondary.main",
+                                width: "100%",
+                                height: "100%",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                flexDirection: "column",
                             }}
                         >
-                            Livro:
+                            <Image sx={{ fontSize: 80 }} />
+                            <Typography>Clique aqui e envie a capa do Ebook</Typography>
+                        </Box>
+                    </Grid>
+                    <Grid xs={7} item pl={1} container>
+                        <Grid item xs={12} mb={2}>
+                            <Typography color="darker.main" variant="h5" fontWeight="bold">
+                                Sinopse
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <DashedInput fullWidth value={sinopse} onChange={handleSinopseInputChange} minRows={15} multiline placeholder="Sinopse" />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h5" color="darker.main" fontWeight="bold">
+                        Especificações
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container>
+                        <Grid xs={6} item container>
+                            <Grid xs={12} item container alignItems="center" py={1}>
+                                <Grid xs={4} item>
+                                    <Typography color="dark.main">Autor: </Typography>
+                                </Grid>
+                                <Grid xs={6} item>
+                                    <DashedInput fullWidth size="small" onChange={handleAutorInputChange} />
+                                </Grid>
+                            </Grid>
+                            <Grid xs={12} item container alignItems="center" py={1}>
+                                <Grid xs={4} item>
+                                    <Typography color="dark.main">Número de páginas: </Typography>
+                                </Grid>
+                                <Grid xs={6} item>
+                                    <DashedInput fullWidth size="small" onChange={handlePagInputChange} />
+                                </Grid>
+                            </Grid>
+                            <Grid xs={12} item container alignItems="center" py={1}>
+                                <Grid xs={4} item>
+                                    <Typography color="dark.main">Ano de lançamento: </Typography>
+                                </Grid>
+                                <Grid xs={6} item>
+                                    <DashedInput fullWidth size="small" onChange={handleAnoInputChange} />
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid xs={6} item container>
+                            <Grid xs={12} item container alignItems="center" py={1}>
+                                <Grid xs={4} item>
+                                    <Typography color="dark.main">Idioma: </Typography>
+                                </Grid>
+                                <Grid xs={6} item>
+                                    <DashedInput fullWidth size="small" onChange={handleIdiomaInputChange} />
+                                </Grid>
+                            </Grid>
+                            <Grid xs={12} item container alignItems="center" py={1}>
+                                <Grid xs={4} item>
+                                    <Typography color="dark.main">Gênero: </Typography>
+                                </Grid>
+                                <Grid xs={6} item>
+                                    <DashedInput fullWidth size="small" onChange={handleGeneroInputChange} />
+                                </Grid>
+                            </Grid>
+                            <Grid xs={12} item container alignItems="center" py={1}>
+                                <Grid xs={4} item>
+                                    <Typography color="dark.main">Formato: </Typography>
+                                </Grid>
+                                <Grid xs={8} item>
+                                    <Typography color="textLight.main">PDF</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid container item xs={12} mt={2}>
+                    <Grid item xs={12}>
+                        <Typography color="darker.main" variant="h5" fontWeight="bold">
+                            Livro: <Typography display="inline">Nenhum arquivo selecionado</Typography>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <input hidden accept="image/*" id="raised-button-file" multiple type="file" />
+                        <label htmlFor="raised-button-file">
+                            <Button color="dark" variant="contained" component="span">
+                                Escolher arquivo
+                            </Button>
                         </label>
-                        <input
-                            type="text"
-                            value={arq}
-                            placeholder="Nenhum arquivo selecionado"
-                            style={{
-                                border: "2px solid #163760",
-                                backgroundColor: "#F1F1F1",
-                                width: "200px",
-                                borderLeft: "none",
-                                borderRight: "none",
-                                borderTop: "none",
-                                outline: "none",
-                            }}
-                            disabled
-                        />
-                    </Box>
-                    <label
-                        class="button_label"
-                        tabIndex="0"
-                        style={{
-                            display: "flex",
-                            height: "35px",
-                            width: "150px",
-                            background: "#163760",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            color: "#FFFFFF",
-                            cursor: "pointer",
-                            fontFamily: "Source Sans Pro, sans-serif",
-                            borderRadius: "5px",
-                            boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-                        }}
-                    >
-                        <input onChange={handleArqInputChange} class="input__button" type="file" accept="pdf/*" style={{ display: "none" }} />
-                        <span class="button">Escolher arquivo</span>
-                    </label>
-                </Box>
-
-                <Box width="flex" height="30vh" display="flex" flexDirection="column" marginTop="10px">
-                    <Box display="flex" alignItems="center" marginBottom="7px">
-                        <label style={{ marginRight: "30px", color: "#163760", fontFamily: "Source Sans Pro, sans-serif", fontWeight: "bold" }}>
-                            Idioma:
-                        </label>
-                        <input
-                            onChange={handleIdiomaInputChange}
-                            type="text"
-                            style={{
-                                border: "1px dotted #163760",
-                                borderRadius: "4px",
-                                backgroundColor: "#F1F1F1",
-                                borderWidth: "2px",
-                                width: "100px",
-                                outline: "none",
-                            }}
-                        />
-                    </Box>
-
-                    <Box display="flex" alignItems="center" marginBottom="7px">
-                        <label style={{ marginRight: "30px", color: "#163760", fontFamily: "Source Sans Pro, sans-serif", fontWeight: "bold" }}>
-                            Gênero:
-                        </label>
-                        <input
-                            onChange={handleGeneroInputChange}
-                            type="text"
-                            style={{
-                                border: "1px dotted #163760",
-                                borderRadius: "4px",
-                                backgroundColor: "#F1F1F1",
-                                borderWidth: "2px",
-                                width: "100px",
-                                outline: "none",
-                            }}
-                        />
-                    </Box>
-                    <Box display="flex" alignItems="center" marginBottom="7px">
-                        <label style={{ marginRight: "24px", color: "#163760", fontFamily: "Source Sans Pro, sans-serif", fontWeight: "bold" }}>
-                            Formato:
-                        </label>
-                        <label style={{ marginRight: "30px", color: "#8f8c8c", fontFamily: "Source Sans Pro, sans-serif", fontWeight: "bold" }}>
-                            PDF
-                        </label>
-                    </Box>
-                </Box>
-            </Box>
-
-            <Box display="flex" alignItems="start" justifyContent="center" marginBottom="20px">
-                <Button onClick={dados} variant="contained" style={{ width: "200px", backgroundColor: "#8CD087", color: "#323131" }}>
-                    Submeter e-Book
-                </Button>
-            </Box>
-        </Box>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} textAlign="center">
+                    <Button onClick={dados} variant="contained" color="success">
+                        Submeter e-Book
+                    </Button>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
