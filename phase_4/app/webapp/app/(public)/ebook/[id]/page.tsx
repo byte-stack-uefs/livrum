67a0aaa4377f8202bda63d79e85bf716e22043de
Almir Neto <almirneto338@gmail.com>
2024-01-05 22:19:22 -0300

    Transformando container de dados do ebook em componente

--- phase_4/app/webapp/app/(public)/ebook/[id]/page.tsx
+++ phase_4/app/webapp/app/(public)/ebook/[id]/page.tsx
@@ -1,22 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import { theme } from "@/app/theme";
-import { useEffect, useState } from "react";
 import Divider from "@/app/components/Divider";
 import Carousel from "@/app/components/Carousel";
-import { AddShoppingCart } from "@mui/icons-material";
-import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import { Button, Container, Tooltip, Typography } from "@mui/material";
-import Ebook from "@/app/interfaces/Ebook";
 import { useCart } from "../../carrinho/useCart";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { Container, Typography } from "@mui/material";
+import EbookDetails from "@/app/components/EbookDetails";
 
 interface EbookPageParams {
     id: number;
-}
-
-interface EbookDetailsProps {
-    ebook: any;
 }
 
 export type CartItemType = {
@@ -30,14 +23,7 @@ export type CartItemType = {
 export default function Page({ params }: { params: EbookPageParams }) {
     const { id } = params;
 
-    const [height, setHeight] = useState(0);
-    const [width, setWidth] = useState(0);
 
-    useEffect(() => {
-        const el = document.querySelector("#ebook-cover-container");
-        setWidth(el?.clientWidth ?? 0);
-        setHeight(el?.clientHeight ?? 0);
-    }, []);
 
     const similars = [
         {
@@ -99,107 +85,9 @@ export default function Page({ params }: { params: EbookPageParams }) {
     return (
         <Container maxWidth={false}>
             <Grid container>
-                <Grid py={2} px={4} container xs={12} sx={{ backgroundColor: "secondary.main", borderRadius: 5, marginY: 6 }}>
-                    <Grid xs={12} textAlign="center" py={2}>
-                        <Typography variant="h4" color="dark.main">
-                            TÍTULO DO EBOOK
-                        </Typography>
-                    </Grid>
-                    <Grid container xs={12} py={2}>
-                        <Grid xs={5} id="ebook-cover-container" p={2}>
-                            <Image
-                                src="https://cdn.kobo.com/book-images/6750d058-29cb-4626-9c12-a62e816a80cc/1200/1200/False/harry-potter-and-the-philosopher-s-stone-3.jpg"
-                                width={width}
-                                height={height}
-                                alt="Ebook cover"
-                                style={{ height: "100%", width: "100%", borderRadius: 30, objectFit: "contain" }}
-                            />
-                        </Grid>
-                        <Grid container xs={7} p={2}>
-                            <Grid xs={12} pt={6}>
-                                <Typography variant="h5" color="dark.main">
-                                    Descrição
-                                </Typography>
-                            </Grid>
-                            <Grid xs={12} sx={{ color: "textLight.main" }}>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus recusandae facere eveniet, magni tempora
-                                    praesentium alias itaque explicabo accusamus asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint
-                                    consequatur?
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus recusandae facere eveniet, magni tempora
-                                    praesentium alias itaque explicabo accusamus asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint
-                                    consequatur?
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus recusandae facere eveniet, magni tempora
-                                    praesentium alias itaque explicabo accusamus asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint
-                                    consequatur?
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At temporibus recusandae facere eveniet, magni tempora
-                                    praesentium alias itaque explicabo accusamus asperiores officiis blanditiis. Natus, tempore esse! Porro alias sint
-                                    consequatur?
-                                </p>
-                            </Grid>
-                            <Grid xs={12} container justifyContent="center" mt={4}>
-                                <Grid container py={3} xs={8} sx={{ backgroundColor: "#c5c5c5", borderRadius: 3 }} textAlign="center" alignSelf="end">
-                                    <Grid xs={6} alignSelf="center">
-                                        <Typography variant="h4" textAlign="center" color="dark.main">
-                                            R$ 39,90
-                                        </Typography>
-                                    </Grid>
-                                    <Grid xs={6} alignSelf="center">
-                                        <Button
-                                            disabled={checkIsProductInCart(ebook)}
-                                            variant="contained"
-                                            onClick={() => handleClickAddCart(ebook)}
-                                            startIcon={<AddShoppingCart />}
-                                        >
-                                            Comprar
-                                        </Button>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Grid container xs={12} px={2}>
-                        <Grid xs={12}>
-                            <Typography variant="h5" color="dark.main">
-                                Especificações
-                            </Typography>
-                        </Grid>
-                        <Grid xs={12} container>
-                            <Grid xs={3}>
-                                <ul style={{ listStyle: "none", padding: 0 }}>
-                                    <li style={{ paddingTop: 4, paddingBottom: 4 }}>
-                                        <ListItem title="Autor" value="X" />
-                                    </li>
-                                    <li style={{ paddingTop: 4, paddingBottom: 4 }}>
-                                        <ListItem title="Número de Páginas" value="192" />
-                                    </li>
-                                    <li style={{ paddingTop: 4, paddingBottom: 4 }}>
-                                        <ListItem title="Ano de lançamento" value="2023" />
-                                    </li>
-                                </ul>
-                            </Grid>
-                            <Grid xs={3}>
-                                <ul style={{ listStyle: "none", padding: 0 }}>
-                                    <li style={{ paddingTop: 4, paddingBottom: 4 }}>
-                                        <ListItem title="Idioma" value="Português" />
-                                    </li>
-                                    <li style={{ paddingTop: 4, paddingBottom: 4 }}>
-                                        <ListItem title="Tamanho" value="8592 KB" />
-                                    </li>
-                                    <li style={{ paddingTop: 4, paddingBottom: 4 }}>
-                                        <ListItem title="Formato" value="PDF" />
-                                    </li>
-                                </ul>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
+
+                <EbookDetails ebook={ebook} onAddCart={handleClickAddCart} shouldDisableAddCart={checkIsProductInCart} />
+
                 <Grid xs={12} container>
                     <Grid xs={12} textAlign="center">
                         <Typography variant="h4" color="dark.main">
@@ -228,11 +116,4 @@ function SimilarEbooks(ebook: any) {
     );
 }
 
-function ListItem({ title, value }: { title: string; value: string }) {
-    return (
-        <span>
-            <span style={{ color: theme.palette.dark.main, fontWeight: "bold" }}>{title}: </span>
-            <span style={{ color: theme.palette.textLight.main, fontWeight: "bold" }}>{value}</span>
-        </span>
-    );
-}
+
