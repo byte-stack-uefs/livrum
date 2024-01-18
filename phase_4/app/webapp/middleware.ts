e8853d30d4b82d1d158ce299922a15b8a8be278c
Almir Neto <almirneto338@gmail.com>
2024-01-17 21:02:21 -0300

    Ativando middleware de acesso

--- phase_4/app/webapp/middleware.ts
+++ phase_4/app/webapp/middleware.ts
@@ -3,7 +3,6 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    return;
     if (request.nextUrl.pathname == "/") {
         return;
     }
