afb3d49362319e064c9878d7becb08c4264686fe
Almir Neto <almirneto338@gmail.com>
2024-01-17 23:22:52 -0300

    Retornando contexto do usuário

--- phase_4/app/webapp/app/context.tsx
+++ phase_4/app/webapp/app/context.tsx
@@ -1,23 +1,29 @@
 "use client";
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const defaultUser = {
     isLoggedIn: false,
     user: {
         id: 1,
-        tipo: '',
-        status: '',
+        tipo: "",
+        status: "",
         nome: "Almir Livrum",
-        email: 'livrum@gmail.com',
+        email: "livrum@gmail.com",
     },
 };
 
 export const Context = createContext(defaultUser);
 
 export default function ContextProvider({ children }: { children: React.ReactNode }) {
-    return (
-        <Context.Provider value={defaultUser}>
-            {children}
-        </Context.Provider>
-    );
+    return <Context.Provider value={defaultUser}>{children}</Context.Provider>;
+}
+
+export function useUser() {
+    const context = useContext(Context);
+
+    if (context == null) {
+        throw new Error("Não foi possível recuperar o Contexto");
+    }
+
+    return context;
 }
