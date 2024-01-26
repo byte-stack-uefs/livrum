0e41e6e9086192f1ab13e4af1b93752796bcdb72
Almir Neto <almirneto338@gmail.com>
2024-01-25 21:30:00 -0300

    Adicionando criação de pix base

--- phase_4/app/api/services/PaymentService.py
+++ phase_4/app/api/services/PaymentService.py
@@ -1,3 +1,4 @@
+from efipay import EfiPay
 from dependencies.settings import Settings
 
 
@@ -7,8 +8,34 @@ class PaymentService:
     def __init__(self):
         self.sts = Settings()
 
+    def _getCredentials(self):
+        return {
+            "client_id": self.sts.efi_client_id,
+            "client_secret": self.sts.efi_client_secret,
+            "sandbox": True,
+            "certificate": "/usr/app/data/certificado.pem",
+        }
+
     def createPix(self):
-        pass
+        efi = EfiPay(self._getCredentials())
+
+        body = {
+            "calendario": {"expiracao": 3600},
+            "valor": {"original": "19.90"},
+            "chave": "bcd6e1ad-4b84-4ab9-adb1-6b16094de84f",
+            "solicitacaoPagador": "Pix referente ao pagamento da compra de Ebooks na Livrum",
+        }
+
+        response = None
+
+        try:
+            response = efi.pix_create_immediate_charge(body=body)
+        except Exception as err:
+            print(err)
+
+        print(response)
+
+        return response
 
     def checkPixPaid(self):
         pass
