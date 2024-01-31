170a4ca67171d7d18b2c0dc01af9fc6a7e874966
Almir Neto <almirneto338@gmail.com>
2024-01-31 14:37:46 -0300

    Adicionando regex para validar dados do cartão

--- phase_4/app/api/models/creditCard.py
+++ phase_4/app/api/models/creditCard.py
@@ -1,19 +1,20 @@
 from enum import Enum
 from pydantic import BaseModel
 from datetime import datetime, date
+from fastapi import Query
+from typing_extensions import Annotated
 import re
 
-class CardType(Enum):
 
+class CardType(Enum):
     VISA = "Visa"
     MASTERCARD = "Mastercard"
     ELO = "Elo"
     UNKNOWN = "Unknown"
 
-class CreditCard:
 
+class CreditCard:
     def __init__(self, **kwargs):
-        
         self.idCard = kwargs.get("idCartao")
         self.namePrinted = kwargs.get("namePrinted")
         self.cardNumber = kwargs.get("cardNumber")
@@ -21,10 +22,9 @@ class CreditCard:
         self.expiryDate = kwargs.get("expiryDate")
         self.idClient = kwargs.get("idClient")
 
+
 class BackToFrontEndCreditCardDTO:
-
     def __init__(self, **kwargs):
-
         self.idCard = kwargs.get("idCartao")
         self.namePrinted = kwargs.get("nomeImpresso")
         self.ultimosDigitos = self._mask_card_number(kwargs.get("numero"))
@@ -32,61 +32,51 @@ class BackToFrontEndCreditCardDTO:
         self.card_type = _classify_card_type(kwargs.get("numero"))
 
     def _mask_card_number(self, card_number):
-
         if card_number:
-
             return f"**** **** **** {card_number[-4:]}"
-            
+
         return None
 
     def _parse_date(self, date_str):
-
         if isinstance(date_str, date):
-
             return date_str.strftime("%m/%Y")
 
         elif date_str:
-
             try:
-
                 parsed_date = datetime.strptime(date_str, "%Y-%m-%d").date()
                 return parsed_date.strftime("%m/%Y")
 
             except ValueError:
-
                 return None
 
         return None
 
     def _mask_card_number(self, card_number):
-
         if card_number:
-
             return f"**** **** **** {card_number[-4:]}"
 
         return None
 
+
 class FrontToBackEndCreditCardDTO(BaseModel):
-    
+    cvv: Annotated[str, Query(pattern=r"^\d{3}$")]
+    cardNumber: Annotated[str, Query(pattern=r"^\d{4}$")]
+    expiryDate: Annotated[str, Query(pattern=r"^\d{4}-\d{2}$", example="2040-11")]
     namePrinted: str
-    cardNumber: str
-    cvv: str
-    expiryDate: str
+
 
 def _classify_card_type(card_number):
-
-    if re.match(r'^4\d{12}(\d{3})?$', card_number):
-
+    if re.match(r"^4\d{12}(\d{3})?$", card_number):
         return CardType.VISA
 
-    elif re.match(r'^(5[1-5]\d{4}|677189)\d{10}$', card_number):
-
+    elif re.match(r"^(5[1-5]\d{4}|677189)\d{10}$", card_number):
         return CardType.MASTERCARD
 
-    elif re.match(r'^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})$', card_number):
-
+    elif re.match(
+        r"^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})$",
+        card_number,
+    ):
         return CardType.ELO
 
     else:
-
-        return CardType.UNKNOWN   
+        return CardType.UNKNOWN
