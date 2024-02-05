9f5e5cd228385b22baf1ac350cd3d210a0e64cd7
Almir Neto <almirneto338@gmail.com>
2024-02-04 21:59:38 -0300

    Corrigindo exceção

--- phase_4/app/webapp/app/components/PixContainer.tsx
+++ phase_4/app/webapp/app/components/PixContainer.tsx
@@ -5,11 +5,11 @@ import qrcode from '@/app/assets/qrcode.png'
 export function PixContainer() {
     return (
         <Grid container>
-            <Grid xs={12} textAlign="center">
+            <Grid item xs={12} textAlign="center">
                 <Image width={400} height={400} src={qrcode} alt="PaymentQrCode" />
             </Grid>
-            <Grid xs={3}></Grid>
-            <Grid xs={12} md={6}>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={12} md={6}>
                 <p style={{ textAlign: 'center' }}>Escaneie o QRCode e realize o pagamento no aplicativo do banco</p>
             </Grid>
         </Grid>
