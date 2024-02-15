46b07c7de9b755ecc52d3f95d20d14877e045da0
Alexjr <88115079+Oguelo@users.noreply.github.com>
2024-02-15 09:26:57 -0300

    Update account.py

--- phase_4/app/api/routers/account.py
+++ phase_4/app/api/routers/account.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated
-from dependencies import security, recoverEmail
-from models.user import User, UserType, RecoveryEmailForm
-from dependencies.recoverEmail import RecoverEmail
+from dependencies import security
+from models.user import User, UserType
 from services.UserService import UserService
-
+import random
+import string
+from services.RecoverEmail import RecoverEmail
 router = APIRouter(prefix="/account", tags=["Account"])
 
 
@@ -21,20 +22,18 @@ def logout():
 @router.get("/isAuthenticated")
 def isAuth(user: Annotated[User, Depends(security.get_current_active_user)]):
     return {"message": "uau"}
-
-
-@router.post("/recuperar-senha")
-def passwordRecover(emailUser: RecoveryEmailForm):
-
-    random_password = RecoverEmail.generateRandomPassword()
-    response = UserService.updatePasswordByEmail(emailUser.email,random_password)
-
+@router.patch("/recuperar-senha")
+def passwordRecover(emailUser: str):
+    length = 12
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    newPass =  ''.join(random.choice(alphabet) for i in range(length))
+    response = UserService.recoverPass(emailUser,newPass)
     if response:
-
-       operation = RecoverEmail.emailRecover(emailUser.email,random_password)
-
-       return operation
-
+       operation = RecoverEmail.emailRecover(emailUser,newPass)
+       return {
+             "status": "success",
+             "message":f"Solicitação de recuperação de senha recebida. Verifique seu e-mail para instruções adicionais."
+            }, 200
     return  {
                 "status": "error",
                 "message": f"Erro ao enviar e-mail de recuperação de senha"
