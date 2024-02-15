050afc2c35b31567e2f60b25c78d71e5fa7c5f4b
Brenda Oliveira <1brendabarbosa@gmail.com>
2024-02-14 21:39:51 -0300

    incluindo funções de ebook ao CartService

--- phase_4/app/api/services/CartService.py
+++ phase_4/app/api/services/CartService.py
@@ -1,6 +1,7 @@
 from database.database import DB
 from models.cart import Cart, CartDTO
-from models.cartItem import CartItemDTO, CartItemForm
+from models.cartItem import CartItemDTO
+from models.ebook import Ebook, EbookModel
 
 class CartService():
     def _convertDTOCart(self, item: dict) -> CartDTO:
@@ -89,6 +90,34 @@ class CartService():
             except:
                 return False
             return True
+        
+    def hasUnavailableEbooks(self, ebooks: List[EbookModel]) -> bool:
+
+        for e in ebooks:
+            if e.isAvailable:
+                return True
+        return False
+
+    def getCartEbooksByUserId(self, id) -> list:
+
+        query = "SELECT \
+                    e.*, u.nome AS nomeAutor	 \
+                FROM \
+                    ebook e \
+                JOIN itemcarrinho i ON \
+                    i.idEBook = e.idEBook \
+                JOIN carrinho c ON \
+                    c.idCarrinho = i.idCarrinho \
+                JOIN autor a ON a.idUsuario = e.idAutor \
+                JOIN usuario u ON a.idUsuario = u.idUsuario \
+                WHERE \
+                    c.idUsuario = %s;"
+        with DB() as db:
+            db.execute(query, [id])
+            data = db.fetchall()
+
+        ebooks = map(lambda x: EbookModel(**x), data)
+        return list(ebooks)
             
 
 
