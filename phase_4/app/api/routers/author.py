7fdfaccbded684fd863bc4765b0c8754e8458e8e
Brenda <1brendabarbosa@gmail.com>
2024-02-16 15:07:39 -0300

    rotas para o get dos cards no dashboard do autor

---
+++ phase_4/app/api/routers/author.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from models.user import UserType
+from dependencies.security import UserHasAccess
+from services.AuthorService import AuthorService
+from models.user import User
+
+access = UserHasAccess([UserType.AUTHOR])
+
+router = APIRouter(prefix="/author", tags=["Authors"])
+service = AuthorService()
+     
+
+@router.get('/faturamento/mensal/<int:mes>')
+def get_faturamento_mensal(mes, user: Annotated[User, Depends(access)]):
+    return service.getFaturamentoMensalAutor(mes=mes, idAutor=user.idUsuario)
+
+@router.get('/vendas/mesal/<int:mes>')
+def get_vendas_mes(mes, user: Annotated[User, Depends(access)]):
+    return service.getTotalUnidadesVendidasMes(mes=mes, idAutor=user.idUsuario)
+
+@router.get('/obras/mesal/<int:mes>')
+def get_obras_criadas_mes(mes, user: Annotated[User, Depends(access)]):
+    return service.getTotalObrasCriadasMes(mes=mes, idAutor=user.idUsuario)
+
