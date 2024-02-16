6a2a73aa2d60a43bde10b810a9339f395c5950b8
Brenda Oliveira <1brendabarbosa@gmail.com>
2024-02-16 03:02:07 -0300

    contagem obras criadas no mes

--- phase_4/app/api/services/AuthorService.py
+++ phase_4/app/api/services/AuthorService.py
@@ -9,7 +9,11 @@ class AuthorService:
     def getFaturamentoAutor(self, idAutor):
         with DB() as db:
             db.execute(
-                "SELECT SUM(ip.valorTotal) AS total_valor FROM pedido p JOIN itempedido ip ON p.idPedido = ip.idPedido JOIN ebook e ON ip.idEbook = e.idEbook WHERE p.status = 'approved' AND e.idAutor = %s;", [idAutor]
+                "SELECT SUM(ip.valorTotal) AS total_valor \
+                FROM pedido p \
+                JOIN itempedido ip ON p.idPedido = ip.idPedido \
+                JOIN ebook e ON ip.idEbook = e.idEbook \
+                WHERE p.status = 'approved' AND e.idAutor = %s;", [idAutor]
             )
             faturamento_total = db.fetchone() 
             
@@ -45,26 +49,49 @@ class AuthorService:
         if idAutor is not None:
             with DB() as db:
                 db.execute(
-                    "SELECT e.idAutor, COUNT(*) AS total_registros\
-                    FROM pedido p\
-                    JOIN itempedido ip ON p.idPedido = ip.idPedido\
-                    JOIN ebook e ON ip.idEbook = e.idEbook\
-                    WHERE p.status = 'approved' AND e.idAutor = %s AND MONTH(p.data) = %s\
+                    "SELECT e.idAutor, COUNT(*) AS total_registros \
+                    FROM pedido p \
+                    JOIN itempedido ip ON p.idPedido = ip.idPedido \
+                    JOIN ebook e ON ip.idEbook = e.idEbook \
+                    WHERE p.status = 'approved' AND e.idAutor = %s AND MONTH(p.data) = %s \
                     GROUP BY e.idAutor;", [idAutor, mes]
                 )
                 data_list = db.fetchone() 
         else:
             with DB() as db:
                 db.execute(
-                    "SELECT e.idAutor, COUNT(*) AS total_registros\
-                    FROM pedido p\
-                    JOIN itempedido ip ON p.idPedido = ip.idPedido\
-                    JOIN ebook e ON ip.idEbook = e.idEbook\
-                    WHERE p.status = 'approved' AND MONTH(p.data) = %s\
+                    "SELECT e.idAutor, COUNT(*) AS total_registros \
+                    FROM pedido p \
+                    JOIN itempedido ip ON p.idPedido = ip.idPedido \
+                    JOIN ebook e ON ip.idEbook = e.idEbook \
+                    WHERE p.status = 'approved' AND MONTH(p.data) = %s \
                     GROUP BY e.idAutor;", [mes]
                 )
                 data_list = db.fetchall() 
 
-        funidades_vendidas_mes = map(self._convertAutorQntDataForm, data_list)
-        return funidades_vendidas_mes
+        unidades_vendidas_mes = map(self._convertAutorQntDataForm, data_list)
+        return unidades_vendidas_mes
 
+    def getTotalObrasCriadasMes(self, mes: int, idAutor=None):
+        if idAutor is not None:
+            with DB() as db:
+                db.execute(
+                    "SELECT idAutor, COUNT(*) AS total_registros \
+                    FROM Ebook \
+                    WHERE idAutor = %s AND MONTH(criadoEm) = %s \
+                    GROUP BY idAutor;", [idAutor, mes]
+                )
+                data_list = db.fetchone() 
+        
+        else:
+            with DB() as db:
+                db.execute(
+                    "SELECT idAutor, COUNT(*) AS total_registros \
+                    FROM Ebook \
+                    WHERE MONTH(criadoEm) = %s \
+                    GROUP BY idAutor;", [mes]
+                )
+                data_list = db.fetchall() 
+        
+        obras_criadas_mes = map(self._convertAutorQntDataForm, data_list)
+        return obras_criadas_mes
