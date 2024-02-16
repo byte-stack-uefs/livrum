718f660711f22a9d6acc00bbbf4b1f12f323f58d
Brenda Oliveira <1brendabarbosa@gmail.com>
2024-02-16 02:04:20 -0300

    dados de faturamento do(s) autor(es)

--- phase_4/app/api/models/author.py
+++ phase_4/app/api/models/author.py
@@ -1,3 +1,8 @@
+from pydantic import BaseModel
 class Author:
     def __init__():
         pass
+
+class AuthorQntDataForm(BaseModel):
+    idUsuario: int
+    data: int
--- phase_4/app/api/services/OrderService.py
+++ phase_4/app/api/services/OrderService.py
@@ -2,9 +2,14 @@ from typing import Union
 from database.database import DB
 from models.ebook import EbookModel
 from models.order import OrderPaymentType, OrderStatus
+from models.author import AuthorQntDataForm
 
 
 class OrderService:
+
+    def _convertAutorDataForm(self, item: dict) -> AuthorQntDataForm:
+        return AuthorQntDataForm(**item)
+
 
     def createOrder(self, customerId, type: OrderPaymentType) -> Union[int | bool]:
 
@@ -58,4 +63,33 @@ class OrderService:
             faturamento_total = db.fetchone() 
             
         return faturamento_total
+    
+    def getFaturamentoMensalAutor(self, mes: int, idAutor=None) -> list[AuthorQntDataForm]:
+        if idAutor is not None:
+            with DB() as db:
+                db.execute(
+                    "SELECT e.idAutor, SUM(ip.valorTotal) AS total_valor \
+                    FROM pedido p JOIN itempedido ip ON p.idPedido = ip.idPedido \
+                    JOIN ebook e ON ip.idEbook = e.idEbook \
+                    WHERE p.status = 'approved' AND e.idAutor = %s AND MONTH(p.data) = %s \
+                    GROUP BY e.idAutor;", [idAutor, mes]
+                )
+                data_list = db.fetchone() 
+                faturamento_mensal_autor= map(self._convertAutorDataForm, data_list)
+            return faturamento_mensal_autor
+        else:
+            #listagem de faturamento de todos os autores
+            with DB() as db:
+                db.execute(
+                    "SELECT e.idAutor, SUM(ip.valorTotal) AS total_valor \
+                    FROM pedido p JOIN itempedido ip ON p.idPedido = ip.idPedido \
+                    JOIN ebook e ON ip.idEbook = e.idEbook \
+                    WHERE p.status = 'approved' AND MONTH(p.data) = %s \
+                    GROUP BY e.idAutor;", [mes]
+                )
+                data_list = db.fetchall() 
+                faturamento_mensal_todos= map(self._convertAutorDataForm, data_list)
+            return faturamento_mensal_todos
+                
+    
 
