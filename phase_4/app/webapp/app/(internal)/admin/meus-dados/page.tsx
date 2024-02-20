6a43d49902f64fd86afb469b757f917b043feabc
Almir Neto <almirneto338@gmail.com>
2024-02-20 19:29:08 -0300

    Removendo campo de senha atual

--- phase_4/app/webapp/app/(internal)/admin/meus-dados/page.tsx
+++ phase_4/app/webapp/app/(internal)/admin/meus-dados/page.tsx
@@ -1,23 +1,15 @@
 "use client";
+
+import { theme } from "@/app/theme";
+import Grid from "@mui/material/Unstable_Grid2";
+import TextField from "@mui/material/TextField";
+import useRequest from "@/app/services/requester";
 import React, { useState, useEffect } from "react";
-import { Button, InputAdornment, Typography, FormControl, IconButton, Alert } from "@mui/material";
+import { UserAttributes } from "@/app/interfaces/User";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import Grid from "@mui/material/Unstable_Grid2";
-import Divider from "@/app/components/Divider";
-import { theme } from "@/app/theme";
-// @ts-ignore
-import BootstrapInput from "@/app/theme";
-import useRequest from '@/app/services/requester';
-import { UserAttributes } from '@/app/interfaces/User';
-import { alpha } from "@mui/material/styles";
+import { Button, InputAdornment, Typography, FormControl, IconButton, Alert } from "@mui/material";
 
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import TextField, { TextFieldProps } from "@mui/material/TextField";
-import { OutlinedInputProps } from "@mui/material/OutlinedInput";
-
-import InputBase from "@mui/material/InputBase";
 const AdminData = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -60,58 +52,59 @@ const AdminData = () => {
 
     useEffect(() => {
         getDataUserObject();
-    }, [])
+    }, []);
 
     const getDataUserObject = () => {
+        requester
+            .get(`/user/`)
+            .then((response: { data: UserAttributes }) => {
+                const userData = response.data;
+                setName(userData.nome);
+                setEmail(userData.email);
+                setOldPassword(userData.senha);
+            })
+            .catch((err: any) => {});
+    };
 
-        requester.get(`/user/`).then((response: { data: UserAttributes; }) => {
-            const userData = response.data;
-            setName(userData.nome);
-            setEmail(userData.email);
-            setOldPassword(userData.senha);
-
-        }).catch((err: any) => { })
-        
-    }
-
-    const handleSubmitClient = async (event: { preventDefault: () => void; }) => {
+    const handleSubmitClient = async (event: { preventDefault: () => void }) => {
         setHasCreationFailed(false);
         setCreationError("");
         setHasCreationSucess(false);
-        setCreationSucess(""); 
+        setCreationSucess("");
         const validationError = validateFields();
         if (validationError) {
             setHasCreationFailed(true);
             setCreationError(validationError);
             return;
         }
-        await requester.patch(`/user/`,{nome: name, email:email, senha:password}).then((response: any) => {
-            getDataUserObject();
-            setHasCreationSucess(true);
-            setCreationSucess("Atualização efetuada com sucesso"); 
-        }).catch((err: any) => {
-            
-            setHasCreationFailed(true);
-            setCreationError("Não foi possível realizar a atualização dos dados"); 
-        
-        })
+        await requester
+            .patch(`/user/`, { nome: name, email: email, senha: password })
+            .then((response: any) => {
+                getDataUserObject();
+                setHasCreationSucess(true);
+                setCreationSucess("Atualização efetuada com sucesso");
+            })
+            .catch((err: any) => {
+                setHasCreationFailed(true);
+                setCreationError("Não foi possível realizar a atualização dos dados");
+            });
     };
 
     const validateFields = () => {
         const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
         const isPasswordValid = password.length >= 4;
-    
+
         if (!isEmailValid) {
             return "Por favor, preencha o e-mail corretamente.";
         }
-    
+
         if (!isPasswordValid) {
             return "A senha deve ter no mínimo 4 caracteres.";
         }
-    
+
         return null;
     };
-   
+
     return (
         <form onSubmit={handleSubmitClient}>
             <Grid xs={11} sm={9} md={7} lg={5} margin="auto">
@@ -163,33 +156,6 @@ const AdminData = () => {
                             />
                         </Grid>
                         <Grid xs={12} sx={{ borderBottom: "1px solid black", marginBottom: 2, marginX: 1 }}></Grid>
-                        <Grid xs={6}>
-                            <div>
-                                <strong>Senha Atual:</strong>
-                            </div>
-                            <TextField
-                                fullWidth
-                                size="small"
-                                type={showPassword ? "text" : "password"}
-                                value={oldPassword}
-                                placeholder={oldPassword}
-                                onChange={(e) => setOldPassword(e.target.value)}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                                onMouseDown={handleMouseDownPassword}
-                                                edge="end"
-                                            >
-                                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        </Grid>
                         <Grid container xs={12}>
                             <Grid xs={6}>
                                 <div>
@@ -247,18 +213,18 @@ const AdminData = () => {
                             </Grid>
                         </Grid>
                         {hasCreationFailed && (
-                                <Grid xs={12}>
-                                    <Alert severity="error" variant="filled" sx={{ width: "100%" }}>
-                                        {creationError}
-                                    </Alert>
-                                </Grid>
+                            <Grid xs={12}>
+                                <Alert severity="error" variant="filled" sx={{ width: "100%" }}>
+                                    {creationError}
+                                </Alert>
+                            </Grid>
                         )}
                         {hasCreationSucess && (
-                        <Grid xs={12}>
-                            <Alert severity="success" variant="filled" sx={{ width: "100%" }}>
+                            <Grid xs={12}>
+                                <Alert severity="success" variant="filled" sx={{ width: "100%" }}>
                                     {creationSucess}
-                            </Alert>
-                        </Grid>
+                                </Alert>
+                            </Grid>
                         )}
                         <Grid xs={12} sm={6} md={4} lg={2}>
                             <Button
